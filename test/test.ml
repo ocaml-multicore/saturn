e22e8e6fcb12857bc37cfa283c8d31c0832feb1b
Nicolas ASSOUAD <nicolas@assouad.com>
2017-04-04 11:20:26 +0100

    New test

--- test/test.ml
+++ test/test.ml
@@ -4,6 +4,12 @@
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
+(*
+########
+Copyright (c) 2017, Nicolas ASSOUAD <nicolas.assouad@ens.fr>
+########
+*)
+
 (*---------------------------------------------------------------------------
    Copyright (c) 2016 KC Sivaramakrishnan
 
@@ -19,3 +25,43 @@
    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
   ---------------------------------------------------------------------------*)
+
+open Printf;;
+
+module Bag = Lf_bag.M;;
+
+let push_test b n init =
+  let rec loop i =
+    if i < n then begin
+      Bag.push b i; loop (i+2)
+    end
+  in loop init
+;;
+
+let rec pop_test b n =
+  if n > 0 then
+    match Bag.pop b with
+    |Some(out) -> print_endline (sprintf "TH%d : %d" (Domain.self ()) out); pop_test b (n-1)
+    |None -> print_endline (sprintf "TH%d : Vide" (Domain.self ())); pop_test b (n-1)
+;;
+
+let rec push_and_pop b n init =
+  let rand1 = Random.int n in
+  let rand2 = Random.int n in
+  push_test b (n + rand1) init;
+  pop_test b (n + rand2);
+  push_and_pop b n init
+;;
+
+let () =
+  let b = Bag.create () in
+  let n1 = 10 in
+  let n2 = 20 in
+
+  Random.self_init ();
+
+  Domain.spawn (fun () -> push_and_pop b n1 0);
+  Domain.spawn (fun () -> push_and_pop b n2 1);
+
+  Unix.sleep 10
+;;
