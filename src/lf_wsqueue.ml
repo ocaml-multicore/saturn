de2716329a601a573e8905648a8633264cf356f6
Nicolas ASSOUAD <nicolas@assouad.com>
2017-04-24 15:47:38 +0100

    Lockfree Work Stealing Queue

---
+++ src/lf_wsqueue.ml
@@ -0,0 +1,156 @@
+(*
+ * Copyright (c) 2015, Théo Laurent <theo.laurent@ens.fr>
+ * Copyright (c) 2015, KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+(*
+########
+Copyright (c) 2017, Nicolas ASSOUAD <nicolas.assouad@ens.fr>
+########
+*)
+
+(* Michael-Scott queue *)
+
+(* TODO KC: Replace with concurrent lock free bag --
+ * http://dl.acm.org/citation.cfm?id=1989550 *)
+
+module Cas = Kcas.W1;;
+
+module type S = sig
+  type 'a t;;
+  val create : 'a -> 'a t;;
+  val is_empty : 'a t -> bool;;
+  val size : 'a t -> int;;
+  val push : 'a t -> 'a -> unit;;
+  val pop : 'a t -> 'a option;;
+  val steal : 'a t -> 'a option;;
+end;;
+
+module CArray = struct
+  type 'a t = 'a Cas.ref array;;
+
+  let create s v = Array.init s (fun i -> Cas.ref v);;
+
+  let size t = Array.length t;;
+
+  let get t i = Cas.get t.(i mod (size t));;
+
+  let put t i v = Cas.set t.(i mod (size t)) v;;
+
+  let grow t top bottom =
+    let s = size t in
+    let ns = 2 * s in
+    let v = get t 0 in
+    let out = create ns v in
+    for i = top to bottom do
+      out.(i mod (size out)) <- t.(i mod (size t))
+    done;
+    out
+  ;;
+
+  let shrink t top bottom =
+    let s = size t in
+    let ns = s / 2 in
+    let v = get t 0 in
+    let out = create ns v in
+    for i = top to bottom do
+      out.(i mod (size out)) <- t.(i mod (size t))
+    done;
+    out
+  ;;
+end;;
+
+
+module M : S = struct
+  let min_size = 16;;
+  let shrink_const = 3;;
+
+  type 'a t = {
+    top : int Cas.ref;
+    bottom : int Cas.ref;
+    tab : 'a CArray.t Cas.ref
+  };;
+
+  let create v = {
+    top = Cas.ref 0;
+    bottom = Cas.ref 0;
+    tab = Cas.ref (CArray.create min_size v)
+  };;
+
+  let is_empty q =
+    let b = Cas.get q.bottom in
+    let t = Cas.get q.top in
+    b - t <= 0
+  ;;
+
+  let size q =
+    let b = Cas.get q.bottom in
+    let t = Cas.get q.top in
+    b - t
+  ;;
+
+  let push q v =
+    let b = Cas.get q.bottom in
+    let t = Cas.get q.top in
+    let a = Cas.get q.tab in
+    let size = b - t in
+    if size >= CArray.size a - 1 then
+      Cas.set q.tab (CArray.grow a t b);
+    CArray.put a b v;
+    Cas.set q.bottom (b+1)
+  ;;
+
+  let pop q =
+    let b = Cas.get q.bottom - 1 in
+    Cas.set q.bottom b;
+    let t = Cas.get q.top in
+    let a = Cas.get q.tab in
+    let size = b - t in
+    if size < 0 then begin
+      Cas.set q.bottom t;
+      None
+    end else
+      let out = Some(CArray.get a b) in
+      if size > 0 then begin
+        if CArray.size a / shrink_const > size then
+          Cas.set q.tab (CArray.shrink a t b);
+        out
+      end else begin
+        Cas.set q.bottom (t+1);
+        if Cas.cas q.top t (t+1) then
+          out
+        else
+          None
+      end
+  ;;
+
+  let rec steal q =
+    let wait = Kcas.Backoff.create () in
+    let t = Cas.get q.top in
+    let b = Cas.get q.bottom in
+    let a = Cas.get q.tab in
+    let size = b - t in
+    if size <= 0 then
+      None
+    else
+      let out = Some(CArray.get a t) in
+      if Cas.cas q.top t (t+1) then
+        out
+      else begin
+        Kcas.Backoff.once wait;
+        steal q
+      end
+  ;;
+end;;
--- test/test.ml
+++ test/test.ml
@@ -28,178 +28,259 @@ Copyright (c) 2017, Nicolas ASSOUAD <nicolas.assouad@ens.fr>
 
 open Printf;;
 
-module Queue = Lf_msqueue.M;;
-module Bag = Lf_bag.M;;
+module Bag_test = struct
+  module Bag = Lf_bag.M;;
 
-module Bst = Lf_mybst.Make(struct
-  type t = int;;
-  let compare = compare;;
-  let hash_t x = (x * 104743) mod 1299721;;
-  (*let hash_t x = x;;*)
-  let str k = sprintf "%d" k;;
-end);;
+  let push_test b n init =
+    let rec loop i =
+      if i < n then begin
+        Bag.push b i; loop (i+2)
+      end
+    in loop init
+  ;;
 
-module MySet = Set.Make(struct
-  type t = int;;
-  let compare = compare;;
-end);;
+  let rec pop_test b n =
+    if n > 0 then
+      match Bag.pop b with
+      |Some(out) -> print_endline (sprintf "TH%d : %d" (Domain.self ()) out); pop_test b (n-1)
+      |None -> print_endline (sprintf "TH%d : Vide" (Domain.self ())); pop_test b (n-1)
+  ;;
 
-let push_test b n init =
-  let rec loop i =
-    if i < n then begin
-      Bag.push b i; loop (i+2)
-    end
-  in loop init
-;;
+  let rec push_and_pop b n init =
+    let rand1 = Random.int n in
+    let rand2 = Random.int n in
+    push_test b (n + rand1) init;
+    pop_test b (n + rand2);
+    push_and_pop b n init
+  ;;
 
-let rec pop_test b n =
-  if n > 0 then
-    match Bag.pop b with
-    |Some(out) -> print_endline (sprintf "TH%d : %d" (Domain.self ()) out); pop_test b (n-1)
-    |None -> print_endline (sprintf "TH%d : Vide" (Domain.self ())); pop_test b (n-1)
-;;
+  let run () =
+    let b = Bag.create () in
+    let n1 = 10 in
+    let n2 = 20 in
 
-let rec push_and_pop b n init =
-  let rand1 = Random.int n in
-  let rand2 = Random.int n in
-  push_test b (n + rand1) init;
-  pop_test b (n + rand2);
-  push_and_pop b n init
-;;
+    Random.self_init ();
 
-let test_bag () =
-  let b = Bag.create () in
-  let n1 = 10 in
-  let n2 = 20 in
+    Domain.spawn (fun () -> push_and_pop b n1 0);
+    Domain.spawn (fun () -> push_and_pop b n2 1);
 
-  Random.self_init ();
-
-  Domain.spawn (fun () -> push_and_pop b n1 0);
-  Domain.spawn (fun () -> push_and_pop b n2 1);
-
-  Unix.sleep 10
-;;
-
-let rand_bool m n =
-  let r = Random.int n in r < m
-;;
-
-let gen_elem nb m =
-  Random.self_init ();
-  let rec loop i out =
-    if i < nb then begin
-      let new_elem = Random.int m in
-      loop (i+1) (new_elem::out)
-    end else
-      out
-  in loop 0 []
-;;
-
-let gen_elem_out l m n =
-  let rec loop l out =
-    match l with
-    |h::t ->
-      if rand_bool m n then
-        loop t (h::out)
-      else
-        loop t out
-    |[] -> out
-  in loop l []
-;;
-
-let gen_queue l =
-  let out = Queue.create () in
-  let rec loop l =
-    match l with
-    |h::t -> Queue.push out h; loop t
-    |[] -> out
-  in loop l; out
-;;
-
-let print_l l f =
-  let rec loop l =
-    match l with
-    |h::t -> print_string (f h); loop t
-    |[] -> print_endline "]"
-  in
-  print_string "[";
-  loop l
-;;
-
-let insert_bst t q =
-  let rec loop () =
-    match Queue.pop q with
-    |Some(v) -> Bst.insert t v v; loop ()
-    |None -> ()
-  in loop ()
-;;
-
-let delete_bst t q =
-  let rec loop () =
-    match Queue.pop q with
-    |Some(v) -> Bst.delete t v; loop ()
-    |None -> ()
-  in loop ()
-;;
-
-let check_elem tree_elems elems_in_s elems_out_s =
-  let rec loop l =
-    match l with
-    |h::t ->
-      if not (MySet.mem h elems_out_s) then
-        loop t
-      else
-        (print_endline (sprintf "Echec %d haven't been deleted !" h); false)
-    |[] -> true
-  in loop tree_elems
-;;
+    Unix.sleep 10
+  ;;
+end;;
 
 
-let test_bst () =
-  let t = Bst.create () in
-  let n = 100000 in
-  let m = 1000000 in
-  let prob_m = 4 in
-  let prob_n = 5 in
-  let thread_insert = 5 in
-  let thread_delete = 5 in
+module BST_test = struct
+  module Queue = Lf_msqueue.M;;
 
-  let elems = gen_elem n m in
-  let elems_out = gen_elem_out elems prob_m prob_n in
-  let elems_s = MySet.of_list elems in
-  let elems_out_s = MySet.of_list elems_out in
-  let queue_in = gen_queue elems in
-  let queue_out = gen_queue elems_out in
+  module Bst = Lf_mybst.Make(struct
+    type t = int;;
+    let compare = compare;;
+    let hash_t x = (x * 104743) mod 1299721;;
+    (*let hash_t x = x;;*)
+    let str k = sprintf "%d" k;;
+  end);;
 
-  print_endline "Insertion Phase";
-  for i = 1 to thread_insert do
-    Domain.spawn (fun () -> insert_bst t queue_in)
-  done;
+  module MySet = Set.Make(struct
+    type t = int;;
+    let compare = compare;;
+  end);;
 
-  Unix.sleep 5;
+  let rand_bool m n =
+    let r = Random.int n in r < m
+  ;;
 
-  print_endline "Deletion Phase";
-  for i = 1 to thread_delete do
-    Domain.spawn (fun () -> delete_bst t queue_out)
-  done;
+  let gen_elem nb m =
+    Random.self_init ();
+    let rec loop i out =
+      if i < nb then begin
+        let new_elem = Random.int m in
+        loop (i+1) (new_elem::out)
+      end else
+        out
+    in loop 0 []
+  ;;
 
-  Unix.sleep 5;
+  let gen_elem_out l m n =
+    let rec loop l out =
+      match l with
+      |h::t ->
+        if rand_bool m n then
+          loop t (h::out)
+        else
+          loop t out
+      |[] -> out
+    in loop l []
+  ;;
 
-  print_endline "Checking Phase";
-  let h = Bst.heigh t in
-  let l = Bst.to_list t in
-  let tree_elems = List.rev_map (fun (hk, k, v) -> k) l in
-  let nature = Bst.still_bst t in
-  let test_success = check_elem tree_elems elems_s elems_out_s in
+  let gen_queue l =
+    let out = Queue.create () in
+    let rec loop l =
+      match l with
+      |h::t -> Queue.push out h; loop t
+      |[] -> out
+    in loop l; out
+  ;;
 
-  print_l l (fun (hk, k, v) -> sprintf "%d ; " hk);
-  print_endline (sprintf "Heigh: %d" h);
-  print_endline (sprintf "Still a BST : %b    Queue IN Empty : %b    Queue OUT Empty : %b" nature (Queue.is_empty queue_in) (Queue.is_empty queue_out));
-  print_endline (sprintf "Test succeed : %b" test_success);
-  ()
-;;
+  let print_l l f =
+    let rec loop l =
+      match l with
+      |h::t -> print_string (f h); loop t
+      |[] -> print_endline "]"
+    in
+    print_string "[";
+    loop l
+  ;;
+
+  let insert_bst t q =
+    let rec loop () =
+      match Queue.pop q with
+      |Some(v) -> Bst.insert t v v; loop ()
+      |None -> ()
+    in loop ()
+  ;;
+
+  let delete_bst t q =
+    let rec loop () =
+      match Queue.pop q with
+      |Some(v) -> Bst.delete t v; loop ()
+      |None -> ()
+    in loop ()
+  ;;
+
+  let check_elem tree_elems elems_in_s elems_out_s =
+    let rec loop l =
+      match l with
+      |h::t ->
+        if not (MySet.mem h elems_out_s) then
+          loop t
+        else
+          (print_endline (sprintf "Echec %d haven't been deleted !" h); false)
+      |[] -> true
+    in loop tree_elems
+  ;;
+
+  let run () =
+    let t = Bst.create () in
+    let n = 100000 in
+    let m = 1000000 in
+    let prob_m = 4 in
+    let prob_n = 5 in
+    let thread_insert = 5 in
+    let thread_delete = 5 in
+
+    let elems = gen_elem n m in
+    let elems_out = gen_elem_out elems prob_m prob_n in
+    let elems_s = MySet.of_list elems in
+    let elems_out_s = MySet.of_list elems_out in
+    let queue_in = gen_queue elems in
+    let queue_out = gen_queue elems_out in
+
+    print_endline "Insertion Phase";
+    for i = 1 to thread_insert do
+      Domain.spawn (fun () -> insert_bst t queue_in)
+    done;
+
+    Unix.sleep 5;
+
+    print_endline "Deletion Phase";
+    for i = 1 to thread_delete do
+      Domain.spawn (fun () -> delete_bst t queue_out)
+    done;
+
+    Unix.sleep 5;
+
+    print_endline "Checking Phase";
+    let h = Bst.heigh t in
+    let l = Bst.to_list t in
+    let tree_elems = List.rev_map (fun (hk, k, v) -> k) l in
+    let nature = Bst.still_bst t in
+    let test_success = check_elem tree_elems elems_s elems_out_s in
+
+    print_l l (fun (hk, k, v) -> sprintf "%d ; " hk);
+    print_endline (sprintf "Heigh: %d" h);
+    print_endline (sprintf "Still a BST : %b    Queue IN Empty : %b    Queue OUT Empty : %b" nature (Queue.is_empty queue_in) (Queue.is_empty queue_out));
+    print_endline (sprintf "Test succeed : %b" test_success);
+    ()
+  ;;
+end;;
+
+
+module Wsqueue_test = struct
+  module Wsqueue = Lf_wsqueue.M;;
+
+  let print q v =
+    match v with
+    |Some(i) -> print_endline (sprintf "Th%d : %d (size %d)" (Domain.self ()) i (Wsqueue.size q))
+    |None -> print_endline (sprintf "Th%d : None (size %d)" (Domain.self ()) (Wsqueue.size q))
+  ;;
+
+  let push_and_pop q n prob_m prob_n =
+    let rec loop i =
+      if i <= n then
+        if Random.int prob_n < prob_m then begin
+          Wsqueue.push q i; loop (i+1)
+        end else begin
+          print q (Wsqueue.pop q); loop i
+        end
+    in loop 1
+  ;;
+
+  let push_n q n =
+    let rec loop i =
+      if i <= n then begin
+        Wsqueue.push q i;
+        loop (i+1)
+      end
+    in loop 1
+  ;;
+
+  let pop_n q n =
+    let rec loop i =
+      if i <= n then begin
+        print q (Wsqueue.pop q);
+        loop (i+1)
+      end
+    in loop 1
+  ;;
+
+  let steal_n q n =
+    let rec loop i =
+      if i <= n then begin
+        print q (Wsqueue.steal q);
+        loop (i+1)
+      end
+    in loop 1
+  ;;
+
+  let easy_test () =
+    let q = Wsqueue.create 0 in
+    let n = 40 in
+    push_n q n;
+    pop_n q (n/2);
+    steal_n q (n/2)
+  ;;
+
+  let run () =
+    let q = Wsqueue.create 0 in
+    let n = 100000 in
+    let prob_m = 1 in
+    let prob_n = 2 in
+    let nb_thread = 2 in
+
+    Domain.spawn (fun () -> push_and_pop q n prob_m prob_n);
+    for i = 1 to nb_thread do
+      Domain.spawn (fun () -> steal_n q n)
+    done;
+
+    Unix.sleep 10;
+
+    ()
+  ;;
+end;;
+
+
 
 let () =
-  (* test_bag () *)
-  test_bst ()
+  (* Bag_test.run () *)
+  Wsqueue_test.run ()
 ;;
