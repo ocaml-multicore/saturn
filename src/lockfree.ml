70f6bd1e4d9ddc791e7c114239096f3867fcce72
model <modelski.bartosz@gmail.com>
2022-10-06 14:57:22 +0100

    add mpmc queue

--- src/lockfree.ml
+++ src/lockfree.ml
@@ -29,3 +29,4 @@ Copyright (c) 2017, Nicolas ASSOUAD <nicolas.assouad@ens.fr>
 module Ws_deque = Ws_deque
 module Spsc_queue = Spsc_queue
 module Mpsc_queue = Mpsc_queue
+module Mpmc_queue = Mpmc_queue
