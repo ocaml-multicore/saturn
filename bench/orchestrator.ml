2fa8c32aa7e34e3c085d60f1ee95a2320886e25f
model <modelski.bartosz@gmail.com>
2022-10-10 22:54:39 +0100

    fix orchestrator bug

--- bench/orchestrator.ml
+++ bench/orchestrator.ml
@@ -2,11 +2,13 @@ type t = { ready : int Atomic.t; total_domains : int; round : int Atomic.t }
 
 let init ~total_domains =
   { ready = Atomic.make 0; total_domains; round = Atomic.make 0 }
+;;
 
-let wait_until_all_ready { ready; total_domains; _ } =
-  while Atomic.get ready < total_domains do
+let wait_until_all_ready ?(round=0) { ready; total_domains; _ } =
+  while Atomic.get ready < total_domains*(round+1) do
     ()
   done
+;;
 
 let worker ({ ready; round; _ } as t) f =
   Atomic.incr ready;
@@ -19,21 +21,23 @@ let worker ({ ready; round; _ } as t) f =
     done;
     f ();
     (* signal that we're done *)
-    Atomic.incr ready
+    Atomic.incr ready;
   done
+;;
 
-let run ({ ready; total_domains; round } as t) ?(drop_first = true) rounds =
+let run ({ ready = _; round; total_domains = _ } as t) ?(drop_first = true) rounds =
   wait_until_all_ready t;
   (* all domains are up, can start benchmarks *)
   let results = ref [] in
   for i = 1 to rounds do
-    Atomic.set ready 0;
+
     let start_time = Unix.gettimeofday () in
     Atomic.incr round;
-    wait_until_all_ready { ready; total_domains; round };
+    wait_until_all_ready ~round:i t;
     let end_time = Unix.gettimeofday () in
+
     let diff = end_time -. start_time in
     if drop_first && i == 1 then () else results := diff :: !results
   done;
   !results
-
+;;
