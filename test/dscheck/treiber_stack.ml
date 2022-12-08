5672cdabd04cc0614fe90b60bbf77066f8493288
Carine Morel <carine@tarides.com>
2022-12-08 12:34:51 +0100

    Add new Qcheck tests and dscheck tests for Michal Scott queue and Treiber stack.

---
+++ test/dscheck/treiber_stack.ml
@@ -0,0 +1,198 @@
+module Atomic = Dscheck.TracedAtomic
+open Lockfree
+
+let drain stack =
+  let remaining = ref 0 in
+  while not (Treiber_stack.is_empty stack) do
+    remaining := !remaining + 1;
+    assert (Option.is_some (Treiber_stack.pop stack))
+  done;
+  !remaining
+
+let producer_consumer () =
+  Atomic.trace (fun () ->
+      let stack = Treiber_stack.create () in
+      let items_total = 3 in
+
+      (* producer *)
+      Atomic.spawn (fun () ->
+          for i = 1 to items_total do
+            Treiber_stack.push stack i
+          done);
+
+      (* consumer *)
+      let popped = ref 0 in
+      Atomic.spawn (fun () ->
+          for _ = 1 to items_total do
+            match Treiber_stack.pop stack with
+            | None -> ()
+            | Some _ -> popped := !popped + 1
+          done);
+
+      (* checks*)
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              let remaining = drain stack in
+              !popped + remaining = items_total)))
+
+let two_producers () =
+  Atomic.trace (fun () ->
+      let stack = Treiber_stack.create () in
+      let items_total = 4 in
+
+      (* two producers *)
+      for i = 0 to 1 do
+        Atomic.spawn (fun () ->
+            for j = 1 to items_total / 2 do
+              (* even nums belong to thr 1, odd nums to thr 2 *)
+              Treiber_stack.push stack (i + (j * 2))
+            done)
+      done;
+
+      (* checks*)
+      Atomic.final (fun () ->
+          let rec get_items s =
+            if Treiber_stack.is_empty s then []
+            else
+              let item = Option.get (Treiber_stack.pop s) in
+              item :: get_items s
+          in
+          let items = get_items stack in
+
+          (* got the same number of items out as in *)
+          Atomic.check (fun () -> items_total = List.length items);
+
+          (* they are in lifo order *)
+          let odd, even = List.partition (fun v -> v mod 2 == 0) items in
+
+          Atomic.check (fun () -> List.sort Int.compare odd = List.rev odd);
+          Atomic.check (fun () -> List.sort Int.compare even = List.rev even)))
+
+let two_consumers () =
+  Atomic.trace (fun () ->
+      let stack = Treiber_stack.create () in
+      let items_total = 4 in
+
+      for i = 1 to items_total do
+        Treiber_stack.push stack i
+      done;
+
+      (* two consumers *)
+      let lists = [ ref []; ref [] ] in
+      List.iter
+        (fun list ->
+          Atomic.spawn (fun () ->
+              for _ = 1 to items_total / 2 do
+                (* even nums belong to thr 1, odd nums to thr 2 *)
+                list := Option.get (Treiber_stack.pop stack) :: !list
+              done)
+          |> ignore)
+        lists;
+
+      (* checks*)
+      Atomic.final (fun () ->
+          let l1 = !(List.nth lists 0) in
+          let l2 = !(List.nth lists 1) in
+
+          (* got the same number of items out as in *)
+          Atomic.check (fun () -> items_total = List.length l1 + List.length l2);
+
+          (* they are in lifo order *)
+          Atomic.check (fun () -> List.sort Int.compare l1 = l1);
+          Atomic.check (fun () -> List.sort Int.compare l2 = l2)))
+
+let two_domains () =
+  Atomic.trace (fun () ->
+      let stack = Treiber_stack.create () in
+      let items_by_domain = 2 in
+
+      (* two producers *)
+      let lists =
+        [
+          (List.init items_by_domain (fun i -> i), ref []);
+          (List.init items_by_domain (fun i -> i + items_by_domain), ref []);
+        ]
+      in
+      List.iter
+        (fun (lpush, lpop) ->
+          Atomic.spawn (fun () ->
+              List.iter
+                (fun elt ->
+                  (* even nums belong to thr 1, odd nums to thr 2 *)
+                  Treiber_stack.push stack elt;
+                  lpop := Option.get (Treiber_stack.pop stack) :: !lpop)
+                lpush)
+          |> ignore)
+        lists;
+
+      (* checks*)
+      Atomic.final (fun () ->
+          let lpop1 = !(List.nth lists 0 |> snd) in
+          let lpop2 = !(List.nth lists 1 |> snd) in
+
+          (* got the same number of items out as in *)
+          Atomic.check (fun () -> items_by_domain = List.length lpop1);
+          Atomic.check (fun () -> items_by_domain = List.length lpop2);
+
+          (* no element are missing *)
+          Atomic.check (fun () ->
+              List.sort Int.compare (lpop1 @ lpop2)
+              = List.init (items_by_domain * 2) (fun i -> i))))
+
+let two_domains_more_pop () =
+  Atomic.trace (fun () ->
+      let stack = Treiber_stack.create () in
+      let items_by_domain = 2 in
+
+      (* two producers *)
+      let lists =
+        [
+          (List.init items_by_domain (fun i -> i), ref []);
+          (List.init items_by_domain (fun i -> i + items_by_domain), ref []);
+        ]
+      in
+      List.iter
+        (fun (lpush, lpop) ->
+          Atomic.spawn (fun () ->
+              List.iter
+                (fun elt ->
+                  Treiber_stack.push stack elt;
+                  lpop := Treiber_stack.pop stack :: !lpop;
+                  lpop := Treiber_stack.pop stack :: !lpop)
+                lpush)
+          |> ignore)
+        lists;
+
+      (* checks*)
+      Atomic.final (fun () ->
+          let lpop1 =
+            !(List.nth lists 0 |> snd)
+            |> List.filter Option.is_some |> List.map Option.get
+          in
+          let lpop2 =
+            !(List.nth lists 1 |> snd)
+            |> List.filter Option.is_some |> List.map Option.get
+          in
+
+          (* got the same number of items out as in *)
+          Atomic.check (fun () ->
+              2 * items_by_domain = List.length lpop1 + List.length lpop2);
+
+          (* no element are missing *)
+          Atomic.check (fun () ->
+              List.sort Int.compare (lpop1 @ lpop2)
+              = List.init (items_by_domain * 2) (fun i -> i))))
+
+let () =
+  let open Alcotest in
+  run "treiber_stack_dscheck"
+    [
+      ( "basic",
+        [
+          test_case "1-producer-1-consumer" `Slow producer_consumer;
+          test_case "2-producers" `Slow two_producers;
+          test_case "2-consumers" `Slow two_consumers;
+          test_case "2-domains" `Slow two_domains;
+          test_case "2-domains-more-pops" `Slow two_domains_more_pop;
+        ] );
+    ]
--- test/qcheck_michael_scott_queue.ml
+++ test/qcheck_michael_scott_queue.ml
@@ -78,12 +78,149 @@ let tests_one_consumer_one_producer =
           fifo && empty);
     ]
 
+let tests_two_domains =
+  QCheck.
+    [
+      (* TEST 1 - two domains doing multiple times one push then one pop.
+         Parallel [push] and [pop].
+      *)
+      Test.make ~count:10_000 ~name:"parallel_pop_push"
+        (pair small_nat small_nat) (fun (npush1, npush2) ->
+          (* Initialization *)
+          let queue = Michael_scott_queue.create () in
+          let sema = Semaphore.Binary.make false in
+
+          (* Using these lists instead of a random one enables to
+             check for more properties. *)
+          let lpush1 = List.init npush1 (fun i -> i) in
+          let lpush2 = List.init npush2 (fun i -> i + npush1) in
+
+          let work lpush =
+            List.map
+              (fun elt ->
+                Michael_scott_queue.push queue elt;
+                Domain.cpu_relax ();
+                Michael_scott_queue.pop queue)
+              lpush
+          in
+
+          let domain1 =
+            Domain.spawn (fun () ->
+                Semaphore.Binary.release sema;
+                work lpush1)
+          in
+          let popped2 =
+            while not (Semaphore.Binary.try_acquire sema) do
+              Domain.cpu_relax ()
+            done;
+            work lpush2
+          in
+
+          (* As a domain always pushs before popping, all pops
+             succeeds. *)
+          let popped1 = Domain.join domain1 |> List.map Option.get in
+          let popped2 = popped2 |> List.map Option.get in
+
+          (* Check 1 : no elements are missing (everyting is popped). *)
+          let all_elt_in =
+            List.sort compare (popped1 @ popped2) = lpush1 @ lpush2
+          in
+          (* filter the elements pushed and popped by domain 1 *)
+          let push1_pop1 = List.filter (fun elt -> elt < npush1) popped1 in
+          (* filter the elements pushed by domain 2 and popped by domain 1 *)
+          let push2_pop1 = List.filter (fun elt -> elt >= npush1) popped1 in
+          (* filter the elements pushed by domain 1 and popped by domain 2 *)
+          let push1_pop2 = List.filter (fun elt -> elt < npush1) popped2 in
+          (* filter the elements pushed and popped by domain 2 *)
+          let push2_pop2 = List.filter (fun elt -> elt >= npush1) popped2 in
+
+          (* all these lists must be sorted *)
+          let is_sorted list = List.sort compare list = list in
+          all_elt_in && is_sorted push1_pop1 && is_sorted push1_pop2
+          && is_sorted push2_pop1 && is_sorted push2_pop2);
+      (* TEST 2 -
+         Parallel [push] and [pop] with two domains
+
+         Two domains randomly pushs and pops in parallel. They stop as
+         soon as they have finished pushing a list of element to
+         push. *)
+      Test.make ~count:10_000 ~name:"parallel_pop_push_random"
+        (pair small_nat small_nat) (fun (npush1, npush2) ->
+          (* Initialization *)
+          let queue = Michael_scott_queue.create () in
+          let sema = Semaphore.Binary.make false in
+
+          let lpush1 = List.init npush1 (fun i -> i) in
+          let lpush2 = List.init npush2 (fun i -> i + npush1) in
+
+          let work lpush =
+            let consecutive_pop = ref 0 in
+            let rec loop lpush popped =
+              let what_to_do = Random.int 2 in
+              if what_to_do = 0 || !consecutive_pop > 10 then (
+                (* randomly choosing between pushing and popping except
+                   if too many consecutive pops have already occurred *)
+                consecutive_pop := 0;
+                match lpush with
+                | [] -> popped
+                | elt :: xs ->
+                    Michael_scott_queue.push queue elt;
+                    loop xs popped)
+              else (
+                incr consecutive_pop;
+                let p = Michael_scott_queue.pop queue in
+                loop lpush (p :: popped))
+            in
+            loop lpush []
+          in
+
+          let domain1 =
+            Domain.spawn (fun () ->
+                Semaphore.Binary.release sema;
+                work lpush1)
+          in
+          let popped2 =
+            while not (Semaphore.Binary.try_acquire sema) do
+              Domain.cpu_relax ()
+            done;
+            work lpush2
+          in
+
+          let popped1 =
+            Domain.join domain1
+            |> List.filter (function None -> false | _ -> true)
+            |> List.map Option.get
+          in
+          let popped2 =
+            popped2
+            |> List.filter (function None -> false | _ -> true)
+            |> List.map Option.get
+          in
+
+          (* Pop everything that is still on the queue *)
+          let popped3 =
+            let rec loop popped =
+              match Michael_scott_queue.pop queue with
+              | None -> popped
+              | Some v -> loop (v :: popped)
+            in
+            loop []
+          in
+          (* Check that no element is missing. *)
+          let all_n_elt_in =
+            List.sort compare (popped1 @ popped2 @ popped3) = lpush1 @ lpush2
+          in
+
+          all_n_elt_in);
+    ]
+
 let main () =
   let to_alcotest = List.map QCheck_alcotest.to_alcotest in
   Alcotest.run "Michael_scott_queue"
     [
       ("test_sequential", to_alcotest tests_sequential);
       ("one_cons_one_prod", to_alcotest tests_one_consumer_one_producer);
+      ("two_domains", to_alcotest tests_two_domains);
     ]
 ;;
 
