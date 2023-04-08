42bd0012a049d9ce8c50c887ff53717fee00303c
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-04-08 13:19:28 +0530

    Better prints, concurrency with do_work function
    Add better print statements, accompanied by a pusher/popper id.
    Add a do_work function that calls Domain.cpu_relax a random large
    number of times. This makes concurrency apparent after the example
    is run.

--- examples/michael_scott_queue.ml
+++ examples/michael_scott_queue.ml
@@ -11,65 +11,64 @@ let single_push_and_pop () =
   | None -> failwith "single_push_and_pop: queue is empty"
   | Some v -> Printf.printf "single_push_and_pop: popped %d\n" v
 
+let do_work () =
+    (* do some work *)
+    for _ = 1 to Random.int 100_000 do
+        Domain.cpu_relax ()
+    done
+
 let concurrent_push () =
   let ms_q = create () in
-  let item = 42 in
 
   (* push concurrently *)
-  let pusher _ =
+  let pusher id item _ =
+    do_work ();
     push ms_q item;
-    Printf.printf "concurrent_push: pushed %d\n" item
+    Printf.printf "concurrent_push: pushed %d (pusher id: %d)\n" item id
   in
-  let domains = Array.init n_domains (fun _ -> Domain.spawn pusher) in
-  Array.iter Domain.join domains;
-
-  (* pop sequentially and examine *)
-  for _ = 1 to n_domains do
-    match pop ms_q with
-    | None -> failwith "concurrent_push: queue is empty"
-    | Some v -> Printf.printf "concurrent_push: popped %d\n" v
-  done
+  let domains = Array.init n_domains (fun i -> Domain.spawn (pusher i (i+1))) in
+  Array.iter Domain.join domains
 
 let concurrent_pop () =
   let ms_q = create () in
-  let item = 42 in
 
   (* push sequentially *)
-  for _ = 1 to n_domains do
-    push ms_q item;
-    Printf.printf "concurrent_pop: pushed %d\n" item
+  for i = 1 to n_domains do
+    push ms_q i
   done;
 
   (* pop concurrently *)
-  let popper _ =
+  let popper id _ =
+    do_work ();
     match pop ms_q with
     | None -> failwith "concurrent_pop: list is empty"
-    | Some v -> Printf.printf "concurrent_pop: popped %d\n" v
+    | Some v -> Printf.printf "concurrent_pop: popped %d (popper id: %d)\n" v id 
   in
-  let domains = Array.init n_domains (fun _ -> Domain.spawn popper) in
+  let domains = Array.init n_domains (fun i -> Domain.spawn (popper i)) in
   Array.iter Domain.join domains
 
 let concurrent_push_and_pop () =
   let ms_q = create () in
-  let item = 42 in
 
   (* push and pop, both concurrently *)
-  let pusher _ =
+  let pusher id item _ =
+    do_work ();
     push ms_q item;
-    Printf.printf "concurrent_push_and_pop: pushed %d\n" item
+    Printf.printf "concurrent_push_and_pop: pushed %d (pusher id: %d)\n" item id
   in
-  let rec pop_one _ =
+  let rec pop_one id _ =
+    do_work ();
     match pop ms_q with
-    | None -> pop_one () (* keep trying until an item is popped *)
-    | Some v -> Printf.printf "concurrent_push_and_pop: popped %d\n" v
+    | None -> pop_one id () (* keep trying until an item is popped *)
+    | Some v -> Printf.printf "concurrent_push_and_pop: popped %d (popper id: %d)\n" v id
   in
 
   (* n_domains/2 pushers, n_domains/2 poppers concurrently *)
   let popper_domains =
-    Array.init (n_domains / 2) (fun _ -> Domain.spawn pop_one)
+    Array.init (n_domains / 2) (fun i -> Domain.spawn (pop_one i))
   in
   let pusher_domains =
-    Array.init (n_domains / 2) (fun _ -> Domain.spawn pusher)
+    Array.init (n_domains / 2) (fun i -> Domain.spawn (pusher i (i+1)))
   in
   Array.iter Domain.join (Array.append pusher_domains popper_domains)
 
