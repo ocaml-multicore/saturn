lyrm/lockfree

Add multicoretest tests for current data structures.

test/treiber_stack/stm_treiber_stack.ml
@@ -1,0 +1,68 @@
+(** Sequential and Parallel model-based tests of treiber_stack *)
+
+open QCheck
+open STM
+module Treiber_stack = Lockfree.Treiber_stack
+
+module WSDConf = struct
+  type cmd = Push of int | Pop | Is_empty
+
+  let show_cmd c =
+    match c with
+    | Push i -> "Push " ^ string_of_int i
+    | Pop -> "Pop"
+    | Is_empty -> "Is_empty"
+
+  type state = int list
+  type sut = int Treiber_stack.t
+
+  let arb_cmd _s =
+    let int_gen = Gen.nat in
+    QCheck.make ~print:show_cmd
+      (Gen.oneof
+         [
+           Gen.map (fun i -> Push i) int_gen;
+           Gen.return Pop;
+           Gen.return Is_empty;
+         ])
+
+  let init_state = []
+  let init_sut () = Treiber_stack.create ()
+  let cleanup _ = ()
+
+  let next_state c s =
+    match c with
+    | Push i -> i :: s
+    | Pop -> ( match s with [] -> s | _ :: s' -> s')
+    | Is_empty -> s
+
+  let precond _ _ = true
+
+  let run c d =
+    match c with
+    | Push i -> Res (unit, Treiber_stack.push d i)
+    | Pop -> Res (option int, Treiber_stack.pop d)
+    | Is_empty -> Res (bool, Treiber_stack.is_empty d)
+
+  let postcond c (s : state) res =
+    match (c, res) with
+    | Push _, Res ((Unit, _), _) -> true
+    | Pop, Res ((Option Int, _), res) -> (
+        match s with [] -> res = None | j :: _ -> res = Some j)
+    | Is_empty, Res ((Bool, _), res) -> (
+        match s with [] -> res | _ -> not res)
+    | _, _ -> false
+end
+
+module WSDT_seq = STM_sequential.Make (WSDConf)
+module WSDT_dom = STM_domain.Make (WSDConf)
+
+let () =
+  let count = 500 in
+  QCheck_base_runner.run_tests_main
+    [
+      WSDT_seq.agree_test ~count
+        ~name:"STM Lockfree.Treiber_stack test sequential";
+      WSDT_dom.agree_test_par ~count
+        ~name:"STM Lockfree.Treiber_stack test parallel";
+    ]

ocaml-multicore/saturn

test/treiber_stack/stm_treiber_stack.ml
@@ -1,0 +1,67 @@
+(** Sequential and Parallel model-based tests of treiber_stack *)
+
+open QCheck
+open STM
+module Treiber_stack = Lockfree.Treiber_stack
+
+module TSConf = struct
+  type cmd = Push of int | Pop | Is_empty
+
+  let show_cmd c =
+    match c with
+    | Push i -> "Push " ^ string_of_int i
+    | Pop -> "Pop"
+    | Is_empty -> "Is_empty"
+
+  type state = int list
+  type sut = int Treiber_stack.t
+
+  let arb_cmd _s =
+    let int_gen = Gen.nat in
+    QCheck.make ~print:show_cmd
+      (Gen.oneof
+         [
+           Gen.map (fun i -> Push i) int_gen;
+           Gen.return Pop;
+           Gen.return Is_empty;
+         ])
+
+  let init_state = []
+  let init_sut () = Treiber_stack.create ()
+  let cleanup _ = ()
+
+  let next_state c s =
+    match c with
+    | Push i -> i :: s
+    | Pop -> ( match s with [] -> s | _ :: s' -> s')
+    | Is_empty -> s
+
+  let precond _ _ = true
+
+  let run c d =
+    match c with
+    | Push i -> Res (unit, Treiber_stack.push d i)
+    | Pop -> Res (option int, Treiber_stack.pop d)
+    | Is_empty -> Res (bool, Treiber_stack.is_empty d)
+
+  let postcond c (s : state) res =
+    match (c, res) with
+    | Push _, Res ((Unit, _), _) -> true
+    | Pop, Res ((Option Int, _), res) -> (
+        match s with [] -> res = None | j :: _ -> res = Some j)
+    | Is_empty, Res ((Bool, _), res) -> res = (s = [])
+    | _, _ -> false
+end
+
+module TS_seq = STM_sequential.Make (TSConf)
+module TS_dom = STM_domain.Make (TSConf)
+
+let () =
+  let count = 500 in
+  QCheck_base_runner.run_tests_main
+    [
+      TS_seq.agree_test ~count
+        ~name:"STM Lockfree.Treiber_stack test sequential";
+      TS_dom.agree_test_par ~count
+        ~name:"STM Lockfree.Treiber_stack test parallel";
+    ]