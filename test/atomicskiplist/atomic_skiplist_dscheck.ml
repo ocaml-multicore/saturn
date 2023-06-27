f7aee5b4665e4266a1bdb35370b6003b313027df
Carine Morel <carine@tarides.com>
2023-06-27 17:41:39 +0200

    Add stm and more dscheck tests.

--- test/atomicskiplist/atomic_skiplist_dscheck.ml
+++ test/atomicskiplist/atomic_skiplist_dscheck.ml
@@ -1,16 +1,97 @@
-(* This dscheck testcase is not terminating. *)
-let two_producers () =
-  Atomic.trace (fun () ->
-      let sl = Atomicskiplist.create 10 in
+open Atomicskiplist
 
-      Atomic.spawn (fun () -> Atomicskiplist.mem sl 2 |> ignore);
-      Atomic.spawn (fun () -> Atomicskiplist.mem sl 3 |> ignore);
+let _two_mem () =
+  Atomic.trace (fun () ->
+      Random.init 0;
+      let sl = create ~max_height:2 () in
+      let added1 = ref false in
+      let found1 = ref false in
+      let found2 = ref false in
+
+      Atomic.spawn (fun () ->
+          added1 := add sl 1;
+          found1 := mem sl 1);
+
+      Atomic.spawn (fun () -> found2 := mem sl 2);
 
       Atomic.final (fun () ->
-          (* Atomic.check (fun () -> !present) *)
-          Atomic.check (fun () -> true)))
+          Atomic.check (fun () -> !added1 && !found1 && not !found2)))
+
+let _two_add () =
+  Atomic.trace (fun () ->
+      Random.init 0;
+      let sl = create ~max_height:2 () in
+      let added1 = ref false in
+      let added2 = ref false in
+
+      Atomic.spawn (fun () -> added1 := add sl 1);
+      Atomic.spawn (fun () -> added2 := add sl 2);
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () -> !added1 && !added2 && mem sl 1 && mem sl 2)))
+
+let _two_add_same () =
+  Atomic.trace (fun () ->
+      Random.init 0;
+      let sl = create ~max_height:2 () in
+      let added1 = ref false in
+      let added2 = ref false in
+
+      Atomic.spawn (fun () -> added1 := add sl 1);
+      Atomic.spawn (fun () -> added2 := add sl 1);
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              (!added1 && not !added2)
+              || (((not !added1) && !added2) && mem sl 1))))
+
+let _two_remove_same () =
+  Atomic.trace (fun () ->
+      Random.init 0;
+      let sl = create ~max_height:1 () in
+      let added1 = ref false in
+      let removed1 = ref false in
+      let removed2 = ref false in
+
+      Atomic.spawn (fun () ->
+          added1 := add sl 1;
+          removed1 := remove sl 1);
+      Atomic.spawn (fun () -> removed2 := remove sl 1);
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              !added1
+              && ((!removed1 && not !removed2) || ((not !removed1) && !removed2))
+              && not (mem sl 1))))
+
+let _two_remove () =
+  Atomic.trace (fun () ->
+      Random.init 0;
+      let sl = create ~max_height:1 () in
+      let added1 = ref false in
+      let removed1 = ref false in
+      let removed2 = ref false in
+
+      Atomic.spawn (fun () ->
+          added1 := add sl 1;
+          removed1 := remove sl 1);
+      Atomic.spawn (fun () -> removed2 := remove sl 2);
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              let found1 = mem sl 1 in
+              !added1 && !removed1 && not !removed2 && not found1)))
 
 let () =
   let open Alcotest in
-  run "atomic_skiplist_dscheck"
-    [ ("basic", [ test_case "2-producers" `Slow two_producers ]) ]
+  run "skiplist_dscheck"
+    [
+      ( "basic",
+        [
+          test_case "2-mem" `Slow _two_mem;
+          test_case "2-add-same" `Slow _two_add_same;
+          test_case "2-add" `Slow _two_add;
+          test_case "2-remove-same" `Slow _two_remove_same;
+          test_case "2-remove" `Slow _two_remove;
+        ] );
+    ]
