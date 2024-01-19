0baab5d5ddd4dc755ef26ac39899e9c81c461ad4
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-01-19 18:40:46 +0200

    Avoid indirection in array and drop `mask` field
    The `mask` can be computed very quickly from the length of the array which is
    known to be a power of two.
    
    Avoiding the indirection means that an allocation, some pointer chasing, and
    potentially a couple of cache misses (an extra object needs to be written by the
    producer (a potential cache miss) and read by consumer (a definite cache miss,
    because the memory is written by the producer)) are avoided per message.  This
    provides a significant performance improvement now that contention has been
    reduced.
    
    Using `Obj.magic ()` with an array that may contain `float` values is prone to
    be problematic and it makes sense to test that things work.

--- src_lockfree/spsc_queue.ml
+++ src_lockfree/spsc_queue.ml
@@ -25,12 +25,11 @@
 module Atomic = Transparent_atomic
 
 type 'a t = {
-  array : 'a Option.t Array.t;
+  array : 'a Array.t;
   tail : int Atomic.t;
   tail_cache : int ref;
   head : int Atomic.t;
   head_cache : int ref;
-  mask : int;
 }
 
 exception Full
@@ -38,42 +37,41 @@ exception Full
 let create ~size_exponent =
   let size = Int.shift_left 1 size_exponent in
   {
-    array = Array.make size None;
+    array = Array.make size (Obj.magic ());
     tail = Atomic.make 0 |> Multicore_magic.copy_as_padded;
     tail_cache = ref 0 |> Multicore_magic.copy_as_padded;
     head = Atomic.make 0 |> Multicore_magic.copy_as_padded;
     head_cache = ref 0 |> Multicore_magic.copy_as_padded;
-    mask = size - 1;
   }
   |> Multicore_magic.copy_as_padded
 
 let push t element =
-  let size = t.mask + 1 in
-  let tail_val = Atomic.fenceless_get t.tail in
+  let size = Array.length t.array in
+  let tail = Atomic.fenceless_get t.tail in
   if
-    !(t.head_cache) + size == tail_val
-    && begin
-         t.head_cache := Atomic.get t.head;
-         !(t.head_cache) + size == tail_val
-       end
+    !(t.head_cache) == tail - size
+    &&
+    let head = Atomic.get t.head in
+    t.head_cache := head;
+    head == tail - size
   then raise Full
   else begin
-    Array.set t.array (tail_val land t.mask) (Some element);
+    Array.unsafe_set t.array (tail land (size - 1)) element;
     Atomic.incr t.tail
   end
 
 let try_push t element =
-  let size = t.mask + 1 in
-  let tail_val = Atomic.fenceless_get t.tail in
+  let size = Array.length t.array in
+  let tail = Atomic.fenceless_get t.tail in
   if
-    !(t.head_cache) + size == tail_val
-    && begin
-         t.head_cache := Atomic.get t.head;
-         !(t.head_cache) + size == tail_val
-       end
+    !(t.head_cache) == tail - size
+    &&
+    let head = Atomic.get t.head in
+    t.head_cache := head;
+    head == tail - size
   then false
   else begin
-    Array.set t.array (tail_val land t.mask) (Some element);
+    Array.unsafe_set t.array (tail land (size - 1)) element;
     Atomic.incr t.tail;
     true
   end
@@ -81,66 +79,60 @@ let try_push t element =
 exception Empty
 
 let pop t =
-  let head_val = Atomic.fenceless_get t.head in
+  let head = Atomic.fenceless_get t.head in
   if
-    head_val == !(t.tail_cache)
-    && begin
-         t.tail_cache := Atomic.get t.tail;
-         head_val == !(t.tail_cache)
-       end
+    head == !(t.tail_cache)
+    &&
+    let tail = Atomic.get t.tail in
+    t.tail_cache := tail;
+    head == tail
   then raise Empty
   else
-    let index = head_val land t.mask in
-    let v = Array.get t.array index in
+    let index = head land (Array.length t.array - 1) in
+    let v = Array.unsafe_get t.array index in
     (* allow gc to collect it *)
-    Array.set t.array index None;
+    Array.unsafe_set t.array index (Obj.magic ());
     Atomic.incr t.head;
-    match v with None -> assert false | Some v -> v
+    v
 
 let pop_opt t =
-  let head_val = Atomic.fenceless_get t.head in
+  let head = Atomic.fenceless_get t.head in
   if
-    head_val == !(t.tail_cache)
-    && begin
-         t.tail_cache := Atomic.get t.tail;
-         head_val == !(t.tail_cache)
-       end
+    head == !(t.tail_cache)
+    &&
+    let tail = Atomic.get t.tail in
+    t.tail_cache := tail;
+    head == tail
   then None
   else
-    let index = head_val land t.mask in
-    let v = Array.get t.array index in
+    let index = head land (Array.length t.array - 1) in
+    let v = Array.unsafe_get t.array index in
     (* allow gc to collect it *)
-    Array.set t.array index None;
+    Array.unsafe_set t.array index (Obj.magic ());
     Atomic.incr t.head;
-    assert (Option.is_some v);
-    v
+    Some v
 
 let peek_opt t =
-  let head_val = Atomic.fenceless_get t.head in
+  let head = Atomic.fenceless_get t.head in
   if
-    head_val == !(t.tail_cache)
-    && begin
-         t.tail_cache := Atomic.get t.tail;
-         head_val == !(t.tail_cache)
-       end
+    head == !(t.tail_cache)
+    &&
+    let tail = Atomic.get t.tail in
+    t.tail_cache := tail;
+    head == tail
   then None
-  else
-    let v = Array.get t.array (head_val land t.mask) in
-    assert (Option.is_some v);
-    v
+  else Some (Array.unsafe_get t.array (head land (Array.length t.array - 1)))
 
 let peek t =
-  let head_val = Atomic.fenceless_get t.head in
+  let head = Atomic.fenceless_get t.head in
   if
-    head_val == !(t.tail_cache)
-    && begin
-         t.tail_cache := Atomic.get t.tail;
-         head_val == !(t.tail_cache)
-       end
+    head == !(t.tail_cache)
+    &&
+    let tail = Atomic.get t.tail in
+    t.tail_cache := tail;
+    head == tail
   then raise Empty
-  else
-    let v = Array.get t.array (head_val land t.mask) in
-    match v with None -> assert false | Some v -> v
+  else Array.unsafe_get t.array (head land (Array.length t.array - 1))
 
 let size t =
   let tail = Atomic.get t.tail in
