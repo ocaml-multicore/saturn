bd8eae00415b7c42c73cb8fdbab80a47e28da9fe
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-01-22 18:05:01 +0200

    "Inline" the `Atomic.t` objects into the `next` fields of nodes
    This effectively halves the length of the linked list of nodes and roughly
    doubles the thruput of the queue.

--- src_lockfree/michael_scott_queue_node.ml
+++ src_lockfree/michael_scott_queue_node.ml
@@ -3,10 +3,12 @@ module Atomic = Transparent_atomic
 type ('a, _) t =
   | Nil : ('a, [> `Nil ]) t
   | Next : {
-      next : ('a, [ `Nil | `Next ]) t Atomic.t;
+      mutable next : ('a, [ `Nil | `Next ]) t;
       mutable value : 'a;
     }
       -> ('a, [> `Next ]) t
 
-let[@inline] make value = Next { next = Atomic.make Nil; value }
-let[@inline] as_atomic (Next r : ('a, [ `Next ]) t) = r.next
+let[@inline] make value = Next { next = Nil; value }
+
+external as_atomic : ('a, [ `Next ]) t -> ('a, [ `Nil | `Next ]) t Atomic.t
+  = "%identity"
