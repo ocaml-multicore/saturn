1b2e1f55d5e7c46d8cda00c0c960c309c240445b
Carine Morel <carine@tarides.com>
2024-05-21 17:22:10 +0200

    Update benchmarks to run spsc_unsafe as well.

--- bench/bench_spsc_queue.ml
+++ bench/bench_spsc_queue.ml
@@ -1,46 +1,88 @@
 open Multicore_bench
-module Queue = Saturn.Single_prod_single_cons_queue
 
-let run_one ~budgetf ?(size_exponent = 3) ?(n_msgs = 80 * Util.iter_factor) () =
-  let t = Queue.create ~size_exponent in
-
-  let before () =
-    while Queue.size t <> 0 do
-      Queue.pop_exn t |> ignore
-    done;
-    let n = Random.int ((1 lsl size_exponent) + 1) in
-    for i = 1 to n do
-      Queue.push_exn t i
-    done
-  in
+let run_one ~unsafe ~budgetf ?(size_exponent = 3)
+    ?(n_msgs = 80 * Util.iter_factor) () =
   let init _ = () in
-  let work i () =
-    if i = 0 then
-      let rec loop n =
-        if 0 < n then
-          if Queue.try_push t n then loop (n - 1)
-          else begin
-            Domain.cpu_relax ();
-            loop n
-          end
+  let work, before =
+    if unsafe then
+      let module Queue = Saturn_lockfree.Single_prod_single_cons_queue_unsafe in
+      let t = Queue.create ~size_exponent in
+
+      let before () =
+        while Queue.size t <> 0 do
+          Queue.pop t |> ignore
+        done;
+        let n = Random.int ((1 lsl size_exponent) + 1) in
+        for i = 1 to n do
+          Queue.push t i
+        done
       in
-      loop n_msgs
+      let work i () =
+        if i = 0 then
+          let rec loop n =
+            if 0 < n then
+              if Queue.try_push t n then loop (n - 1)
+              else begin
+                Domain.cpu_relax ();
+                loop n
+              end
+          in
+          loop n_msgs
+        else
+          let rec loop n =
+            if 0 < n then
+              match Queue.pop_opt t with
+              | Some _ -> loop (n - 1)
+              | None ->
+                  Domain.cpu_relax ();
+                  loop n
+          in
+          loop n_msgs
+      in
+      (work, before)
     else
-      let rec loop n =
-        if 0 < n then
-          match Queue.pop_opt t with
-          | Some _ -> loop (n - 1)
-          | None ->
-              Domain.cpu_relax ();
-              loop n
+      let module Queue = Saturn_lockfree.Single_prod_single_cons_queue in
+      let t = Queue.create ~size_exponent in
+
+      let before () =
+        while Queue.size t <> 0 do
+          Queue.pop_exn t |> ignore
+        done;
+        let n = Random.int ((1 lsl size_exponent) + 1) in
+        for i = 1 to n do
+          Queue.push_exn t i
+        done
       in
-      loop n_msgs
+      let work i () =
+        if i = 0 then
+          let rec loop n =
+            if 0 < n then
+              if Queue.try_push t n then loop (n - 1)
+              else begin
+                Domain.cpu_relax ();
+                loop n
+              end
+          in
+          loop n_msgs
+        else
+          let rec loop n =
+            if 0 < n then
+              match Queue.pop_opt t with
+              | Some _ -> loop (n - 1)
+              | None ->
+                  Domain.cpu_relax ();
+                  loop n
+          in
+          loop n_msgs
+      in
+      (work, before)
   in
 
   let config = Printf.sprintf "2 workers, capacity %d" (1 lsl size_exponent) in
   Times.record ~budgetf ~n_domains:2 ~before ~init ~work ()
   |> Times.to_thruput_metrics ~n:n_msgs ~singular:"message" ~config
 
-let run_suite ~budgetf =
+let run_suite ~unsafe ~budgetf =
   [ 0; 3; 6; 9; 12; 15 ]
-  |> List.concat_map @@ fun size_exponent -> run_one ~budgetf ~size_exponent ()
+  |> List.concat_map @@ fun size_exponent ->
+     run_one ~budgetf ~size_exponent ~unsafe ()
