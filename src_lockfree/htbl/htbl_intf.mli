ac9a14da05a33161a32218be9db92a2850338a31
Carine Morel <carine@tarides.com>
2024-10-29 11:32:35 +0100

    mdx tests for documentation.

--- src_lockfree/htbl/htbl_intf.mli
+++ src_lockfree/htbl/htbl_intf.mli
@@ -1,16 +1,4 @@
 module type HTBL = sig
-  (** Lock-free hash table.
-
-    The operations provided by this hash table are designed to work as building
-    blocks of non-blocking algorithms.  Specifically, the operation signatures
-    and semantics are designed to allow building
-    {{:https://dl.acm.org/doi/10.1145/62546.62593} consensus protocols over
-    arbitrary numbers of processes}.
-
-    🏎️ Single key reads with this hash table are actually wait-free rather than
-    just lock-free.  Internal resizing automatically uses all the threads that
-    are trying to write to the hash table. *)
-
   (** {1 API} *)
 
   type (!'k, !'v) t
@@ -30,12 +18,12 @@ module type HTBL = sig
     table.
 
     - The optional [hashed_type] argument can and usually should be used to
-      specify the [equal] and [hash] operations on keys.  Slow polymorphic
+      specify the [equal] and [hash] operations on keys. Slow polymorphic
       equality [(=)] and slow polymorphic {{!Stdlib.Hashtbl.seeded_hash} [seeded_hash (Bits64.to_int (Random.bits64 ()))]}
-      is used by default.
-    - The default [min_buckets] is unspecified and a given [min_buckets] may be
+      are used by default.
+    - The default [min_buckets] is unspecified, and a given [min_buckets] may be
       adjusted by the implementation.
-    - The default [max_buckets] is unspecified and a given [max_buckets] may be
+    - The default [max_buckets] is unspecified, and a given [max_buckets] may be
       adjusted by the implementation. *)
 
   val hashed_type_of : ('k, 'v) t -> 'k hashed_type
@@ -43,16 +31,16 @@ module type HTBL = sig
     table [htbl] was created. *)
 
   val min_buckets_of : ('k, 'v) t -> int
-  (** [min_buckets_of htbl] returns the minimum number of buckets of the hash
+  (** [min_buckets_of htbl] returns the minimum number of buckets in the hash
     table [htbl].
 
-    ℹ️ The returned value may not be the same as given to {!create}. *)
+    ℹ️ The returned value may not be the same as the one given to {!create}. *)
 
   val max_buckets_of : ('k, 'v) t -> int
-  (** [max_buckets_of htbl] returns the maximum number of buckets of the hash
+  (** [max_buckets_of htbl] returns the maximum number of buckets in the hash
     table [htbl].
 
-    ℹ️ The returned value may not be the same as given to {!create}. *)
+    ℹ️ The returned value may not be the same as the one given to {!create}. *)
 
   val length : ('k, 'v) t -> int
   (** [length htbl] returns the number of bindings in the hash table [htbl]. *)
@@ -63,7 +51,7 @@ module type HTBL = sig
   (** [find_exn htbl key] returns the current binding of [key] in the hash table
     [htbl] or raises {!Not_found} if no such binding exists.
 
-    @raise Not_found in case no binding of [key] exists in the hash table
+    @raise Not_found if no binding of [key] exists in the hash table
       [htbl]. *)
 
   val mem : ('k, 'v) t -> 'k -> bool
@@ -74,45 +62,45 @@ module type HTBL = sig
 
   val try_add : ('k, 'v) t -> 'k -> 'v -> bool
   (** [try_add htbl key value] tries to add a new binding of [key] to [value] to
-    the hash table [htbl].  Returns [true] on success and [false] in case the
-    hash table already contained a binding for [key]. *)
+    the hash table [htbl]. Returns [true] on success and [false] if the
+    hash table already contains a binding for [key]. *)
 
   (** {2 Updating bindings} *)
 
   val try_set : ('k, 'v) t -> 'k -> 'v -> bool
   (** [try_set htbl key value] tries to update an existing binding of [key] to
-    [value] in the hash table [htbl].  Returns [true] on success and [false] in
-    case the hash table did not contain a binding for [key]. *)
+    [value] in the hash table [htbl]. Returns [true] on success and [false] if
+    the hash table does not contain a binding for [key]. *)
 
   val try_compare_and_set : ('k, 'v) t -> 'k -> 'v -> 'v -> bool
   (** [try_compare_and_set htbl key before after] tries to update an existing
     binding of [key] from the [before] value to the [after] value in the hash
-    table [htbl].  Returns [true] on success and [false] in case the hash table
-    did not contain a binding of [key] to the [before] value.
+    table [htbl]. Returns [true] on success and [false] if the hash table
+    does not contain a binding of [key] to the [before] value.
 
     ℹ️ The values are compared using physical equality, i.e. the [==]
     operator. *)
 
   val set_exn : ('k, 'v) t -> 'k -> 'v -> 'v
   (** [set_exn htbl key after] tries to update an existing binding of [key] from
-    some [before] value to the [after] value in the hash table [htbl].  Returns
+    some [before] value to the [after] value in the hash table [htbl]. Returns
     the [before] value on success or raises {!Not_found} if no such binding
     exists.
 
-    @raise Not_found in case no binding of [key] exists in the hash table
+    @raise Not_found if no binding of [key] exists in the hash table
       [htbl]. *)
 
   (** {2 Removing bindings} *)
 
   val try_remove : ('k, 'v) t -> 'k -> bool
   (** [try_remove htbl key] tries to remove a binding of [key] from the hash table
-    [htbl].  Returns [true] on success and [false] in case the hash table did
-    not contain a binding for [key]. *)
+    [htbl]. Returns [true] on success and [false] if the hash table does not
+    contain a binding for [key]. *)
 
   val try_compare_and_remove : ('k, 'v) t -> 'k -> 'v -> bool
   (** [try_compare_and_remove htbl key before] tries to remove a binding of [key]
-    to the [before] value from the hash table [htbl].  Returns [true] on success
-    and [false] in case the hash table did not contain a binding of [key] to the
+    to the [before] value from the hash table [htbl]. Returns [true] on success
+    and [false] if the hash table does not contain a binding of [key] to the
     [before] value.
 
     ℹ️ The values are compared using physical equality, i.e. the [==]
@@ -120,10 +108,10 @@ module type HTBL = sig
 
   val remove_exn : ('k, 'v) t -> 'k -> 'v
   (** [remove_exn htbl key] tries to remove a binding of [key] to some [before]
-    value from the hash table [htbl].  Returns the [before] value on success or
+    value from the hash table [htbl]. Returns the [before] value on success or
     raises {!Not_found} if no such binding exists.
 
-    @raise Not_found in case no binding of [key] exists in the hash table
+    @raise Not_found if no binding of [key] exists in the hash table
       [htbl]. *)
 
   (** {2 Examining contents} *)
@@ -132,24 +120,24 @@ module type HTBL = sig
   (** [to_seq htbl] takes a snapshot of the bindings in the hash table [htbl] and
     returns them as an association sequence.
 
-    🐌 This is a linear time operation. *)
+    🐌 This is a linear-time operation. *)
 
   val remove_all : ('k, 'v) t -> ('k * 'v) Seq.t
   (** [remove_all htbl] takes a snapshot of the bindings in the hash table [htbl],
     removes the bindings from the hash table, and returns the snapshot as an
     association sequence.
 
-    🐌 This is a linear time operation. *)
+    🐌 This is a linear-time operation. *)
 
   val find_random_exn : ('k, 'v) t -> 'k
   (** [find_random_exn htbl] tries to find a random binding from the hash table
-    [htbl] and returns the key of the binding or raises {!Not_found} in case the
+    [htbl] and returns the key of the binding or raises {!Not_found} if the
     hash table is empty.
 
-    🐌 This is an expected constant time operation with worst case linear time
+    🐌 This is an expected constant-time operation with worst-case linear-time
     complexity.
 
-    @raise Not_found in case the hash table [htbl] is empty. *)
+    @raise Not_found if the hash table [htbl] is empty. *)
 
   (** {1 Examples}
 
