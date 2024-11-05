lyrm/lockfree

Add new functions and improve documentations of Treiber stack.

src_lockfree/treiber_stack.ml
@@ -3,8 +3,83 @@
 type 'a node = Nil | Cons of { value : 'a; tail : 'a node }
 type 'a t = 'a node Atomic.t
 
-let create () = Atomic.make Nil |> Multicore_magic.copy_as_padded
+let create () = Atomic.make_contended Nil
 let is_empty t = Atomic.get t == Nil
+
+let of_seq seq =
+  Seq.fold_left
+    (fun (len, acc) elt -> (len + 1, Cons { value = elt; tail = acc }))
+    (1, Nil) seq
+  |> snd |> Atomic.make_contended
+
+(* *)
+
+exception Empty
+
+type ('a, _) poly =
+  | Option : ('a, 'a option) poly
+  | Value : ('a, 'a) poly
+  | Unit : ('a, unit) poly
+
+let rec pop_as : type a r. a t -> Backoff.t -> (a, r) poly -> r =
+ fun t backoff poly ->
+  match Atomic.get t with
+  | Nil -> begin
+      match poly with Option -> None | Value | Unit -> raise_notrace Empty
+    end
+  | Cons cons_r as cons ->
+      if Atomic.compare_and_set t cons cons_r.tail then
+        match poly with
+        | Option -> Some cons_r.value
+        | Value -> cons_r.value
+        | Unit -> ()
+      else pop_as t (Backoff.once backoff) poly
+
+let pop_exn t = pop_as t Backoff.default Value
+let pop_opt t = pop_as t Backoff.default Option
+let drop_exn t = pop_as t Backoff.default Unit
+
+type ('a, _) poly_peek =
+  | Option : ('a, 'a option) poly_peek
+  | Value : ('a, 'a) poly_peek
+
+let peek_as : type a r. a t -> (a, r) poly_peek -> r =
+ fun t poly ->
+  match Atomic.get t with
+  | Nil -> begin
+      match poly with Option -> None | Value -> raise_notrace Empty
+    end
+  | Cons cons -> (
+      match poly with Option -> Some cons.value | Value -> cons.value)
+
+let peek_exn t = peek_as t Value
+let peek_opt t = peek_as t Option
+
+let rec pop_all t backoff =
+  match Atomic.get t with
+  | Nil -> []
+  | old_head ->
+      if Atomic.compare_and_set t old_head Nil then
+        let[@tail_mod_cons] rec aux = function
+          | Nil -> []
+          | Cons cons -> cons.value :: aux cons.tail
+        in
+        aux old_head
+      else pop_all t (Backoff.once backoff)
+
+let pop_all t = pop_all t Backoff.default
+
+let to_seq t =
+  match Atomic.get t with
+  | Nil -> Seq.empty
+  | old_head ->
+      let rec aux s () =
+        match s with
+        | Nil -> Seq.Nil
+        | Cons cons -> Seq.Cons (cons.value, aux cons.tail)
+      in
+      aux old_head
+(* *)
 
 let rec push t value backoff =
   let tail = Atomic.get t in
@@ -14,18 +89,66 @@ let rec push t value backoff =
 
 let push t value = push t value Backoff.default
 
-exception Empty
-
-type ('a, _) poly = Option : ('a, 'a option) poly | Value : ('a, 'a) poly
-
-let rec pop_as : type a r. a t -> Backoff.t -> (a, r) poly -> r =
- fun t backoff poly ->
+let rec push_all_ t backoff values =
+  let rec build_node acc = function
+    | [] -> acc
+    | x :: xs -> build_node (Cons { tail = acc; value = x }) xs
+  in
   match Atomic.get t with
-  | Nil -> begin match poly with Option -> None | Value -> raise Empty end
-  | Cons cons_r as cons ->
-      if Atomic.compare_and_set t cons cons_r.tail then
-        match poly with Option -> Some cons_r.value | Value -> cons_r.value
-      else pop_as t (Backoff.once backoff) poly
+  | Nil ->
+      if Atomic.compare_and_set t Nil (build_node Nil values) then ()
+      else push_all_ t (Backoff.once backoff) values
+  | Cons _ as old_head ->
+      if Atomic.compare_and_set t old_head @@ build_node old_head values then ()
+      else push_all_ t (Backoff.once backoff) values
 
-let pop t = pop_as t Backoff.default Value
-let pop_opt t = pop_as t Backoff.default Option
+let push_all t values = push_all_ t Backoff.default values
+let add_seq t seq = push_all_ t Backoff.default (List.of_seq seq)
+
+(* *)
+let try_compare_and_pop t value =
+  let rec aux backoff =
+    match Atomic.get t with
+    | Nil -> false
+    | Cons cons_r as old_head ->
+        if cons_r.value == value then
+          if Atomic.compare_and_set t old_head cons_r.tail then true
+          else aux (Backoff.once backoff)
+        else false
+  in
+  aux Backoff.default
+
+let try_compare_and_set t old_value new_value =
+  let rec aux backoff =
+    match Atomic.get t with
+    | Nil -> false
+    | Cons cons_r as old_head ->
+        if cons_r.value == old_value then
+          if
+            Atomic.compare_and_set t old_head
+            @@ Cons { cons_r with value = new_value }
+          then true
+          else aux (Backoff.once backoff)
+        else false
+  in
+  aux Backoff.default
+
+let rec set_exn t value backoff =
+  match Atomic.get t with
+  | Nil -> raise_notrace Empty
+  | Cons cons_r as old_head ->
+      if Atomic.compare_and_set t old_head @@ Cons { cons_r with value } then
+        value
+      else set_exn t value (Backoff.once backoff)
+
+let set_exn t value = set_exn t value Backoff.default
+
+let rec try_set t value backoff =
+  match Atomic.get t with
+  | Nil -> false
+  | Cons cons_r as old_head ->
+      if Atomic.compare_and_set t old_head @@ Cons { cons_r with value } then
+        true
+      else try_set t value (Backoff.once backoff)
+
+let try_set t value = try_set t value Backoff.default

test/treiber_stack/stm_treiber_stack.ml
@@ -1,20 +1,44 @@
-(** Sequential and Parallel model-based tests of treiber_stack *)
+(** Sequential and Parallel model-based tests of Treiber stack *)
 
 open QCheck
 open STM
-module Treiber_stack = Saturn_lockfree.Stack
+module Stack = Saturn_lockfree.Stack
 
 module Spec = struct
-  type cmd = Push of int | Pop | Is_empty
+  type cmd =
+    | Push of int
+    | Push_all of int list (* add_seq uses the same function as push_all *)
+    | Pop_opt
+    (* peek_exn and drop_exn use the same function as pop_exn*)
+    | Pop_all
+    | Peek_opt
+    (* peek_exn uses the same function as peek_exn *)
+    | Try_compare_and_pop of int
+    | Try_compare_and_set of int * int
+    | Try_set of int
+    (* set_exn uses the same function as try_set *)
+    | Is_empty
+    | To_seq
+
+  let string_of_int_list l =
+    "[" ^ String.concat "; " (List.map string_of_int l) ^ "]"
 
   let show_cmd c =
     match c with
     | Push i -> "Push " ^ string_of_int i
-    | Pop -> "Pop"
+    | Push_all l -> "Push_all " ^ string_of_int_list l
+    | Pop_opt -> "Pop_opt"
+    | Pop_all -> "Pop_all"
+    | Peek_opt -> "Peek_opt"
+    | Try_compare_and_pop i -> "Try_compare_and_pop " ^ string_of_int i
+    | Try_compare_and_set (i, j) ->
+        "Try_compare_and_set (" ^ string_of_int i ^ ", " ^ string_of_int j ^ ")"
+    | Try_set i -> "Try_set " ^ string_of_int i
     | Is_empty -> "Is_empty"
+    | To_seq -> "To_seq"
 
   type state = int list
-  type sut = int Treiber_stack.t
+  type sut = int Stack.t
 
   let arb_cmd _s =
     let int_gen = Gen.nat in
@@ -22,37 +46,66 @@ module Spec = struct
       (Gen.oneof
          [
            Gen.map (fun i -> Push i) int_gen;
-           Gen.return Pop;
+           Gen.map (fun l -> Push_all l) (Gen.list int_gen);
+           Gen.return Pop_opt;
+           Gen.return Pop_all;
+           Gen.return Peek_opt;
+           Gen.map (fun i -> Try_compare_and_pop i) int_gen;
+           Gen.map2 (fun i j -> Try_compare_and_set (i, j)) int_gen int_gen;
+           Gen.map (fun i -> Try_set i) int_gen;
            Gen.return Is_empty;
+           Gen.return To_seq;
          ])
 
   let init_state = []
-  let init_sut () = Treiber_stack.create ()
+  let init_sut () = Stack.create ()
   let cleanup _ = ()
 
   let next_state c s =
     match c with
     | Push i -> i :: s
-    | Pop -> ( match s with [] -> s | _ :: s' -> s')
+    | Push_all l -> List.rev l @ s
+    | Pop_opt -> ( match s with [] -> s | _ :: s' -> s')
+    | Pop_all -> []
+    | Peek_opt -> s
+    | Try_compare_and_pop i -> (
+        match s with [] -> [] | hd :: tl -> if hd = i then tl else s)
+    | Try_compare_and_set (i, j) -> (
+        match s with [] -> [] | hd :: tl -> if hd = i then j :: tl else s)
+    | Try_set i -> ( match s with [] -> s | _ :: tl -> i :: tl)
     | Is_empty -> s
+    | To_seq -> s
 
   let precond _ _ = true
 
   let run c d =
     match c with
-    | Push i -> Res (unit, Treiber_stack.push d i)
-    | Pop -> Res (result int exn, protect Treiber_stack.pop d)
-    | Is_empty -> Res (bool, Treiber_stack.is_empty d)
+    | Push i -> Res (unit, Stack.push d i)
+    | Push_all l -> Res (unit, Stack.push_all d l)
+    | Pop_opt -> Res (option int, Stack.pop_opt d)
+    | Pop_all -> Res (list int, Stack.pop_all d)
+    | Peek_opt -> Res (option int, Stack.peek_opt d)
+    | Try_compare_and_pop i -> Res (bool, Stack.try_compare_and_pop d i)
+    | Try_compare_and_set (i, j) -> Res (bool, Stack.try_compare_and_set d i j)
+    | Try_set i -> Res (bool, Stack.try_set d i)
+    | Is_empty -> Res (bool, Stack.is_empty d)
+    | To_seq -> Res (seq int, Stack.to_seq d)
 
   let postcond c (s : state) res =
     match (c, res) with
     | Push _, Res ((Unit, _), _) -> true
-    | Pop, Res ((Result (Int, Exn), _), res) -> (
-        match s with
-        | [] -> res = Error Treiber_stack.Empty
-        | j :: _ -> res = Ok j)
+    | Push_all _, Res ((Unit, _), _) -> true
+    | Try_compare_and_pop i, Res ((Bool, _), res) -> (
+        match s with [] -> res = false | hd :: _ -> res = (hd = i))
+    | Try_compare_and_set (i, _), Res ((Bool, _), res) -> (
+        match s with [] -> res = false | hd :: _ -> res = (hd = i))
+    | Try_set _, Res ((Bool, _), res) -> res = (s <> [])
+    | (Pop_opt | Peek_opt), Res ((Option Int, _), res) -> (
+        match s with [] -> res = None | j :: _ -> res = Some j)
+    | Pop_all, Res ((List Int, _), res) -> res = s
     | Is_empty, Res ((Bool, _), res) -> res = (s = [])
+    | To_seq, Res ((Seq Int, _), res) -> List.of_seq res = s
     | _, _ -> false
 end
 
-let () = Stm_run.run ~name:"Saturn_lockfree.Treiber_stack" (module Spec) |> exit
+let () = Stm_run.run ~name:"Saturn_lockfree.Bounded_stack" (module Spec) |> exit

ocaml-multicore/saturn

src/treiber_stack.ml
@@ -3,8 +3,83 @@
 type 'a node = Nil | Cons of { value : 'a; tail : 'a node }
 type 'a t = 'a node Atomic.t
 
-let create () = Atomic.make Nil |> Multicore_magic.copy_as_padded
+let create () = Atomic.make_contended Nil
 let is_empty t = Atomic.get t == Nil
+
+let of_list list =
+  List.fold_left (fun acc elt -> Cons { value = elt; tail = acc }) Nil list
+  |> Atomic.make_contended
+
+let of_seq seq =
+  Seq.fold_left (fun acc elt -> Cons { value = elt; tail = acc }) Nil seq
+  |> Atomic.make_contended
+
+(* *)
+
+exception Empty
+
+type ('a, _) poly = Option : ('a, 'a option) poly | Value : ('a, 'a) poly
+
+let peek_as : type a r. a t -> (a, r) poly -> r =
+ fun t poly ->
+  match Atomic.get t with
+  | Nil -> begin
+      match poly with Option -> None | Value -> raise_notrace Empty
+    end
+  | Cons cons -> (
+      match poly with Option -> Some cons.value | Value -> cons.value)
+
+let peek_exn t = peek_as t Value
+let peek_opt t = peek_as t Option
+
+type ('a, _) poly2 =
+  | Option : ('a, 'a option) poly2
+  | Value : ('a, 'a) poly2
+  | Unit : ('a, unit) poly2
+
+let rec pop_as : type a r. a t -> Backoff.t -> (a, r) poly2 -> r =
+ fun t backoff poly ->
+  match Atomic.get t with
+  | Nil -> begin
+      match poly with Option -> None | Value | Unit -> raise_notrace Empty
+    end
+  | Cons cons_r as cons ->
+      if Atomic.compare_and_set t cons cons_r.tail then
+        match poly with
+        | Option -> Some cons_r.value
+        | Value -> cons_r.value
+        | Unit -> ()
+      else pop_as t (Backoff.once backoff) poly
+
+let pop_exn t = pop_as t Backoff.default Value
+let pop_opt t = pop_as t Backoff.default Option
+let drop_exn t = pop_as t Backoff.default Unit
+
+let rec pop_all t backoff =
+  match Atomic.get t with
+  | Nil -> []
+  | old_head ->
+      if Atomic.compare_and_set t old_head Nil then
+        let[@tail_mod_cons] rec aux = function
+          | Nil -> []
+          | Cons cons -> cons.value :: aux cons.tail
+        in
+        aux old_head
+      else pop_all t (Backoff.once backoff)
+
+let pop_all t = pop_all t Backoff.default
+
+let to_seq t =
+  match Atomic.get t with
+  | Nil -> Seq.empty
+  | old_head ->
+      let rec aux s () =
+        match s with
+        | Nil -> Seq.Nil
+        | Cons cons -> Seq.Cons (cons.value, aux cons.tail)
+      in
+      aux old_head
+(* *)
 
 let rec push t value backoff =
   let tail = Atomic.get t in
@@ -14,18 +89,65 @@ let rec push t value backoff =
 
 let push t value = push t value Backoff.default
 
-exception Empty
+(**)
 
-type ('a, _) poly = Option : ('a, 'a option) poly | Value : ('a, 'a) poly
+type ('a, _) poly3 = Value : ('a, 'a) poly3 | Bool : ('a, bool) poly3
 
-let rec pop_as : type a r. a t -> Backoff.t -> (a, r) poly -> r =
- fun t backoff poly ->
+let rec set_as : type v r. v t -> v -> Backoff.t -> (v, r) poly3 -> r =
+ fun t value backoff poly ->
   match Atomic.get t with
-  | Nil -> begin match poly with Option -> None | Value -> raise Empty end
-  | Cons cons_r as cons ->
-      if Atomic.compare_and_set t cons cons_r.tail then
-        match poly with Option -> Some cons_r.value | Value -> cons_r.value
-      else pop_as t (Backoff.once backoff) poly
+  | Nil -> ( match poly with Value -> raise_notrace Empty | Bool -> false)
+  | Cons cons_r as old_head ->
+      if Atomic.compare_and_set t old_head @@ Cons { cons_r with value } then
+        match poly with Value -> cons_r.value | Bool -> true
+      else set_as t value (Backoff.once backoff) poly
 
-let pop t = pop_as t Backoff.default Value
-let pop_opt t = pop_as t Backoff.default Option
+let set_exn t value = set_as t value Backoff.default Value
+let try_set t value = set_as t value Backoff.default Bool
+
+(**)
+
+let rec push_all_ t backoff values =
+  let rec build_node acc = function
+    | [] -> acc
+    | x :: xs -> build_node (Cons { tail = acc; value = x }) xs
+  in
+  match Atomic.get t with
+  | Nil ->
+      if Atomic.compare_and_set t Nil (build_node Nil values) then ()
+      else push_all_ t (Backoff.once backoff) values
+  | Cons _ as old_head ->
+      if Atomic.compare_and_set t old_head @@ build_node old_head values then ()
+      else push_all_ t (Backoff.once backoff) values
+
+let push_all t values =
+  match values with [] -> () | _ -> push_all_ t Backoff.default values
+
+let add_seq t seq = push_all_ t Backoff.default (List.of_seq seq)
+
+(* *)
+
+type op = Set | Pop
+
+let try_compare_and_ t old_value new_value op =
+  let rec aux backoff =
+    match Atomic.get t with
+    | Nil -> false
+    | Cons cons_r as old_head ->
+        if cons_r.value == old_value then
+          if
+            Atomic.compare_and_set t old_head
+            @@
+            match op with
+            | Set -> Cons { cons_r with value = new_value }
+            | Pop -> cons_r.tail
+          then true
+          else aux (Backoff.once backoff)
+        else false
+  in
+  aux Backoff.default
+
+let try_compare_and_pop t value = try_compare_and_ t value value Pop
+
+let try_compare_and_set t old_value new_value =
+  try_compare_and_ t old_value new_value Set

test/treiber_stack/stm_treiber_stack.ml
@@ -1,20 +1,45 @@
-(** Sequential and Parallel model-based tests of treiber_stack *)
+(** Sequential and Parallel model-based tests of bounded_queue *)
 
 open QCheck
 open STM
-module Treiber_stack = Saturn.Stack
+module Stack = Saturn.Stack
 
 module Spec = struct
-  type cmd = Push of int | Pop | Is_empty
+  type cmd =
+    | Push of int
+    | Push_all of int list
+    | Pop_opt
+    (* peek_exn and drop_exn use the same function as pop_exn*)
+    | Pop_all
+    | Peek_opt
+    (* peek_exn uses the same function as peek_exn *)
+    | Try_compare_and_pop of int
+    | Try_compare_and_set of int * int
+    | Set_exn of int
+    (* try_set uses the same function as set_exn *)
+    | To_seq
+    | Is_empty
+
+  let string_of_int_list l =
+    "[" ^ String.concat "; " (List.map string_of_int l) ^ "]"
 
   let show_cmd c =
     match c with
     | Push i -> "Push " ^ string_of_int i
-    | Pop -> "Pop"
+    | Push_all l -> "Push_all " ^ string_of_int_list l
+    (* | Push_all_exn l -> "Push_all_exn " ^ string_of_int_list l *)
+    | Pop_opt -> "Pop_opt"
+    | Pop_all -> "Pop_all"
+    | Peek_opt -> "Peek_opt"
+    | Try_compare_and_pop i -> "Try_compare_and_pop " ^ string_of_int i
+    | Try_compare_and_set (i, j) ->
+        "Try_compare_and_set (" ^ string_of_int i ^ ", " ^ string_of_int j ^ ")"
+    | To_seq -> "To_seq"
+    | Set_exn i -> "Try_set " ^ string_of_int i
     | Is_empty -> "Is_empty"
 
   type state = int list
-  type sut = int Treiber_stack.t
+  type sut = int Stack.t
 
   let arb_cmd _s =
     let int_gen = Gen.nat in
@@ -22,35 +47,67 @@ module Spec = struct
       (Gen.oneof
          [
            Gen.map (fun i -> Push i) int_gen;
-           Gen.return Pop;
+           Gen.map (fun l -> Push_all l) (Gen.list int_gen);
+           Gen.return Pop_opt;
+           Gen.return Pop_all;
+           Gen.return Peek_opt;
+           Gen.map (fun i -> Try_compare_and_pop i) int_gen;
+           Gen.map2 (fun i j -> Try_compare_and_set (i, j)) int_gen int_gen;
+           Gen.map (fun i -> Set_exn i) int_gen;
+           Gen.return To_seq;
            Gen.return Is_empty;
          ])
 
   let init_state = []
-  let init_sut () = Treiber_stack.create ()
+  let init_sut () = Stack.create ()
   let cleanup _ = ()
 
   let next_state c s =
     match c with
     | Push i -> i :: s
-    | Pop -> ( match s with [] -> s | _ :: s' -> s')
+    | Push_all l -> List.rev l @ s
+    (* | Push_all_exn l ->
+        if List.length s + List.length l > capacity then s else List.rev l @ s *)
+    | Pop_opt -> ( match s with [] -> s | _ :: s' -> s')
+    | Pop_all -> []
+    | Peek_opt -> s
+    | Try_compare_and_pop i -> (
+        match s with [] -> [] | hd :: tl -> if hd = i then tl else s)
+    | Try_compare_and_set (i, j) -> (
+        match s with [] -> [] | hd :: tl -> if hd = i then j :: tl else s)
+    | Set_exn i -> ( match s with [] -> s | _ :: tl -> i :: tl)
+    | To_seq -> s
     | Is_empty -> s
 
   let precond _ _ = true
 
   let run c d =
     match c with
-    | Push i -> Res (unit, Treiber_stack.push d i)
-    | Pop -> Res (result int exn, protect Treiber_stack.pop d)
-    | Is_empty -> Res (bool, Treiber_stack.is_empty d)
+    | Push i -> Res (unit, Stack.push d i)
+    | Push_all l -> Res (unit, Stack.push_all d l)
+    | Pop_opt -> Res (option int, Stack.pop_opt d)
+    | Pop_all -> Res (list int, Stack.pop_all d)
+    | Peek_opt -> Res (option int, Stack.peek_opt d)
+    | Try_compare_and_pop i -> Res (bool, Stack.try_compare_and_pop d i)
+    | Try_compare_and_set (i, j) -> Res (bool, Stack.try_compare_and_set d i j)
+    | Set_exn i -> Res (result int exn, protect (fun d -> Stack.set_exn d i) d)
+    | To_seq -> Res (seq int, Stack.to_seq d)
+    | Is_empty -> Res (bool, Stack.is_empty d)
 
   let postcond c (s : state) res =
     match (c, res) with
-    | Push _, Res ((Unit, _), _) -> true
-    | Pop, Res ((Result (Int, Exn), _), res) -> (
-        match s with
-        | [] -> res = Error Treiber_stack.Empty
-        | j :: _ -> res = Ok j)
+    | Push _, Res ((Unit, _), ()) -> true
+    | Push_all _, Res ((Unit, _), _) -> true
+    | Try_compare_and_pop i, Res ((Bool, _), res) -> (
+        match s with [] -> res = false | hd :: _ -> res = (hd = i))
+    | Try_compare_and_set (i, _), Res ((Bool, _), res) -> (
+        match s with [] -> res = false | hd :: _ -> res = (hd = i))
+    | Set_exn _, Res ((Result (Int, Exn), _), res) -> (
+        match s with [] -> res = Error Stack.Empty | x :: _ -> res = Ok x)
+    | (Pop_opt | Peek_opt), Res ((Option Int, _), res) -> (
+        match s with [] -> res = None | j :: _ -> res = Some j)
+    | Pop_all, Res ((List Int, _), res) -> res = s
+    | To_seq, Res ((Seq Int, _), res) -> List.of_seq res = s
     | Is_empty, Res ((Bool, _), res) -> res = (s = [])
     | _, _ -> false
 end