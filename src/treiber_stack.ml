0e05bc2546beeac6b533f9de27a67d146be83350
Carine Morel <carine@tarides.com>
2024-11-12 20:52:35 +0100

    Applied reviews.

--- src/treiber_stack.ml
+++ src/treiber_stack.ml
@@ -1,18 +1,11 @@
 (** Treiber's Lock Free stack *)
 
-type 'a node = Nil | Cons of { value : 'a; tail : 'a node }
-type 'a t = 'a node Atomic.t
+type 'a t = 'a list Atomic.t
 
-let create () = Atomic.make_contended Nil
-let is_empty t = Atomic.get t == Nil
-
-let of_list list =
-  List.fold_left (fun acc elt -> Cons { value = elt; tail = acc }) Nil list
-  |> Atomic.make_contended
-
-let of_seq seq =
-  Seq.fold_left (fun acc elt -> Cons { value = elt; tail = acc }) Nil seq
-  |> Atomic.make_contended
+let create () = Atomic.make_contended []
+let is_empty t = Atomic.get t == []
+let of_list list = Atomic.make_contended list
+let of_seq seq = Atomic.make_contended (List.of_seq seq)
 
 (* *)
 
@@ -23,11 +16,8 @@ type ('a, _) poly = Option : ('a, 'a option) poly | Value : ('a, 'a) poly
 let peek_as : type a r. a t -> (a, r) poly -> r =
  fun t poly ->
   match Atomic.get t with
-  | Nil -> begin
-      match poly with Option -> None | Value -> raise_notrace Empty
-    end
-  | Cons cons -> (
-      match poly with Option -> Some cons.value | Value -> cons.value)
+  | [] -> begin match poly with Option -> None | Value -> raise Empty end
+  | hd :: _ -> ( match poly with Option -> Some hd | Value -> hd)
 
 let peek_exn t = peek_as t Value
 let peek_opt t = peek_as t Option
@@ -40,15 +30,12 @@ type ('a, _) poly2 =
 let rec pop_as : type a r. a t -> Backoff.t -> (a, r) poly2 -> r =
  fun t backoff poly ->
   match Atomic.get t with
-  | Nil -> begin
-      match poly with Option -> None | Value | Unit -> raise_notrace Empty
+  | [] -> begin
+      match poly with Option -> None | Value | Unit -> raise Empty
     end
-  | Cons cons_r as cons ->
-      if Atomic.compare_and_set t cons cons_r.tail then
-        match poly with
-        | Option -> Some cons_r.value
-        | Value -> cons_r.value
-        | Unit -> ()
+  | hd :: tail as before ->
+      if Atomic.compare_and_set t before tail then
+        match poly with Option -> Some hd | Value -> hd | Unit -> ()
       else pop_as t (Backoff.once backoff) poly
 
 let pop_exn t = pop_as t Backoff.default Value
@@ -57,34 +44,21 @@ let drop_exn t = pop_as t Backoff.default Unit
 
 let rec pop_all t backoff =
   match Atomic.get t with
-  | Nil -> []
+  | [] -> []
   | old_head ->
-      if Atomic.compare_and_set t old_head Nil then
-        let[@tail_mod_cons] rec aux = function
-          | Nil -> []
-          | Cons cons -> cons.value :: aux cons.tail
-        in
-        aux old_head
+      if Atomic.compare_and_set t old_head [] then old_head
       else pop_all t (Backoff.once backoff)
 
 let pop_all t = pop_all t Backoff.default
 
 let to_seq t =
-  match Atomic.get t with
-  | Nil -> Seq.empty
-  | old_head ->
-      let rec aux s () =
-        match s with
-        | Nil -> Seq.Nil
-        | Cons cons -> Seq.Cons (cons.value, aux cons.tail)
-      in
-      aux old_head
+  match Atomic.get t with [] -> Seq.empty | old_head -> List.to_seq old_head
 (* *)
 
 let rec push t value backoff =
-  let tail = Atomic.get t in
-  let cons = Cons { value; tail } in
-  if not (Atomic.compare_and_set t tail cons) then
+  let before = Atomic.get t in
+  let after = value :: before in
+  if not (Atomic.compare_and_set t before after) then
     push t value (Backoff.once backoff)
 
 let push t value = push t value Backoff.default
@@ -96,10 +70,10 @@ type ('a, _) poly3 = Value : ('a, 'a) poly3 | Bool : ('a, bool) poly3
 let rec set_as : type v r. v t -> v -> Backoff.t -> (v, r) poly3 -> r =
  fun t value backoff poly ->
   match Atomic.get t with
-  | Nil -> ( match poly with Value -> raise_notrace Empty | Bool -> false)
-  | Cons cons_r as old_head ->
-      if Atomic.compare_and_set t old_head @@ Cons { cons_r with value } then
-        match poly with Value -> cons_r.value | Bool -> true
+  | [] -> ( match poly with Value -> raise Empty | Bool -> false)
+  | hd :: tail as old_head ->
+      if Atomic.compare_and_set t old_head @@ (value :: tail) then
+        match poly with Value -> hd | Bool -> true
       else set_as t value (Backoff.once backoff) poly
 
 let set_exn t value = set_as t value Backoff.default Value
@@ -108,22 +82,20 @@ let try_set t value = set_as t value Backoff.default Bool
 (**)
 
 let rec push_all_ t backoff values =
-  let rec build_node acc = function
-    | [] -> acc
-    | x :: xs -> build_node (Cons { tail = acc; value = x }) xs
-  in
   match Atomic.get t with
-  | Nil ->
-      if Atomic.compare_and_set t Nil (build_node Nil values) then ()
+  | [] ->
+      if Atomic.compare_and_set t [] values then ()
       else push_all_ t (Backoff.once backoff) values
-  | Cons _ as old_head ->
-      if Atomic.compare_and_set t old_head @@ build_node old_head values then ()
+  | _ as old_head ->
+      if Atomic.compare_and_set t old_head (values @ old_head) then ()
       else push_all_ t (Backoff.once backoff) values
 
 let push_all t values =
-  match values with [] -> () | _ -> push_all_ t Backoff.default values
+  match values with
+  | [] -> ()
+  | _ -> push_all_ t Backoff.default (List.rev values)
 
-let add_seq t seq = push_all_ t Backoff.default (List.of_seq seq)
+let add_seq t seq = push_all_ t Backoff.default (List.of_seq seq |> List.rev)
 
 (* *)
 
@@ -132,15 +104,12 @@ type op = Set | Pop
 let try_compare_and_ t old_value new_value op =
   let rec aux backoff =
     match Atomic.get t with
-    | Nil -> false
-    | Cons cons_r as old_head ->
-        if cons_r.value == old_value then
+    | [] -> false
+    | hd :: tl as old_head ->
+        if hd == old_value then
           if
             Atomic.compare_and_set t old_head
-            @@
-            match op with
-            | Set -> Cons { cons_r with value = new_value }
-            | Pop -> cons_r.tail
+            @@ match op with Set -> new_value :: tl | Pop -> tl
           then true
           else aux (Backoff.once backoff)
         else false
--- src/treiber_stack.mli
+++ src/treiber_stack.mli
@@ -22,16 +22,7 @@ val is_empty : 'a t -> bool
 exception Empty
 (** Raised when {!pop_exn}, {!peek_exn}, {!drop_exn}, or {!set_exn} is 
   applied to an empty stack.
-
-  This exception is meant to avoid allocations required by an option type.
-  As such, it does not register backtrace information and it is recommended to 
-  use the following pattern to catch it.
-
-  {@ocaml skip[
-  match pop_exn s with
-    | value -> () (* ... *) 
-    | exception Empty -> () (* ... *)
-  ]} *)
+*)
 
 val peek_exn : 'a t -> 'a
 (** [peek_exn stack] returns the top element of the [stack] without removing it.
