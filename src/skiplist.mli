b2b957aa61255315e2a957cca36ce1f658d3aec8
Carine Morel <carine@tarides.com>
2024-11-26 20:30:34 +0100

    Doc improvement.

--- src/skiplist.mli
+++ src/skiplist.mli
@@ -1,5 +1,7 @@
 (** A lock-free skiplist. *)
 
+(** {1 API}*)
+
 type (!'k, !'v) t
 (** The type of a lock-free skiplist containing bindings of keys of type ['k] to
     values of type ['v]. *)
@@ -14,11 +16,17 @@ val create : ?max_height:int -> compare:('k -> 'k -> int) -> unit -> ('k, 'v) t
 
     The optional [max_height] argument determines the maximum height of nodes in
     the skiplist and directly affects the performance of the skiplist.  The
-    current implementation does not adjust height automatically. *)
+    current implementation does not adjust height automatically.
+    [max_height] can be more than 30. *)
 
 val max_height_of : ('k, 'v) t -> int
 (** [max_height_of s] returns the maximum height of nodes of the skiplist [s] as
     specified to {!create}. *)
+
+val length : ('k, 'v) t -> int
+(** [length s] computes the number of bindings in the skiplist [s]. *)
+
+(** {2 Looking up bindings} *)
 
 val find_opt : ('k, 'v) t -> 'k -> 'v option
 (** [find_opt s k] tries to find a binding of [k] to [v] from the skiplist [s]
@@ -27,6 +35,8 @@ val find_opt : ('k, 'v) t -> 'k -> 'v option
 
 val mem : ('k, 'v) t -> 'k -> bool
 (** [mem s k] determines whether the skiplist [s] contained a binding of [k]. *)
+
+(** {2 Adding bindings} *)
 
 val try_add : ('k, 'v) t -> 'k -> 'v -> bool
 (** [try_add s k v] tries to add a new binding of [k] to [v] into the skiplist
@@ -38,5 +48,63 @@ val try_remove : ('k, 'v) t -> 'k -> bool
     returns [true] on success.  Otherwise the skiplist did not contain a binding
     of [k] and [false] is returned. *)
 
-val length : ('k, 'v) t -> int
-(** [length s] computes the number of bindings in the skiplist [s]. *)
+(** {1 Examples} *)
+
+(** {2 Sequential example} 
+
+{[
+    # open Saturn.Skiplist
+    # let t = create ~compare:Int.compare ()
+    val t : (int, '_weak1) t = <abstr>
+    # try_add t 42 "The answer"
+    - : bool = true
+
+    # try_add t 101 "Basics"
+    - : bool = true
+
+    # find_opt t 42
+    - : string option = Some "The answer"
+
+    # try_add t 101 "The basics"
+    - : bool = false
+
+    # try_remove t 101
+    - : bool = true
+]}
+*)
+
+(** {2 Multicore example}
+
+  **Note**: The barrier is used in this example solely to make the results more
+  interesting by increasing the likelihood of parallelism. Spawning a domain is 
+  a costly operation, especially compared to the relatively small amount of work
+  being performed here. In practice, using a barrier in this manner is unnecessary.
+
+{[
+    # open Saturn.Skiplist
+    # let t : (int, int) t= create ~compare:Int.compare ()
+    val t : (int, int) t = <abstr>
+    # Random.self_init ()
+    - : unit = ()
+    # let barrier = Atomic.make 2
+    val barrier : int Atomic.t = <abstr>
+
+    # let work () = 
+        Atomic.decr barrier;
+        while Atomic.get barrier > 0 do () done;
+        for i = 0 to 10 do
+            if Random.bool () then 
+                try_add t i i |> ignore
+            else 
+                try_remove t i |> ignore
+        done
+    val work : unit -> unit = <fun>
+
+    # let d1 = Domain.spawn work
+    val d1 : unit Domain.t = <abstr>
+    # let d2 = Domain.spawn work
+    val d2 : unit Domain.t = <abstr>
+    # Domain.join d1; Domain.join d2
+    - : unit = ()
+]}
+*)
