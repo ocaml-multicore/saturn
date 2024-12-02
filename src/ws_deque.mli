2bb4d4bd86e6a0e5436dc674a074a9a6d8072ea3
Carine Morel <carine@tarides.com>
2024-12-02 09:58:01 +0100

    Ws deque update : documentation improvement and drop functions (#168)
     Improve documentation, add of_list, drop_exn and steal_drop.

--- src/ws_deque.mli
+++ src/ws_deque.mli
@@ -1,17 +1,19 @@
 (** Lock-free single-producer, multi-consumer dynamic-size double-ended queue (deque).
 
-    The main strength of deque in a typical work-stealing setup with per-core structure
-    is efficient work distribution. Owner uses [push] and [pop] method to operate at
-    one end of the deque, while other (free) cores can efficiently steal work on the
-    other side.
+      The main strength of a deque in a typical work-stealing setup with a 
+      per-core structure, is efficient work distribution. The owner uses [push] 
+      and [pop] methods to operate at one end of the deque, while other (free) 
+      cores can efficiently steal work from the other side.
 
-    This approach is great for throughput. Stealers and owner working on different sides
-    reduces contention in work distribution. Further, local LIFO order runs related tasks
-    one after one improves locality.
+      This approach is great for throughput. Stealers and the owner working on
+      different sides, reduce contention in work distribution. Further, the 
+      local LIFO order, running related tasks one after another, improves locality.
 
-    On the other hand, the local LIFO order does not offer any fairness guarantees.
-    Thus, it is not the best choice when tail latency matters.
+      On the other hand, the local LIFO order does not offer any fairness 
+      guarantees. Thus, it is not the best choice when tail latency matters.
 *)
+
+(** {1 API} *)
 
 type 'a t
 (** Type of work-stealing queue *)
@@ -19,37 +21,136 @@ type 'a t
 val create : unit -> 'a t
 (** [create ()] returns a new empty work-stealing queue. *)
 
+val of_list : 'a list -> 'a t
+(** [of_list list] creates a new work-stealing queue from [list].
+
+  🐌 This is a linear-time operation.
+
+ {[
+        # open Saturn.Work_stealing_deque
+        # let t : int t = of_list [1;2;3;4]
+        val t : int t = <abstr>
+        # pop_opt t
+        - : int option = Some 4
+        # pop_opt t 
+        - : int option = Some 3
+ ]}
+*)
+
 exception Empty
 
-(** {1 Queue owner functions} *)
+(** {2 Queue owner functions} *)
 
 val push : 'a t -> 'a -> unit
-(** [push t v] adds [v] to the front of the queue [q].
-      It should only be invoked by the domain which owns the queue [q]. *)
+(** [push queue element] adds [element] at the end of the [queue].
+      It should only be invoked by the domain that owns the [queue]. *)
 
 val pop_exn : 'a t -> 'a
-(** [pop_exn q] removes and returns the first element in queue
-      [q].It should only be invoked by the domain which owns the queue
-      [q].
+(** [pop_exn queue] removes and returns the last element of the [queue]. It 
+      should only be invoked by the domain that owns the [queue].
 
-      @raise Exit if the queue is empty.
-      *)
+       @raises Empty if the [queue] is empty. *)
 
 val pop_opt : 'a t -> 'a option
-(** [pop_opt q] removes and returns the first element in queue [q], or
-    returns [None] if the queue is empty.  *)
+(** [pop_opt queue] removes and returns [Some] of the last element of the 
+      [queue], or returns [None] if the [queue] is empty. It should only 
+      be invoked by the domain that owns the [queue]. *)
 
-(** {1 Stealers function} *)
+val drop_exn : 'a t -> unit
+(** [drop_exn queue] removes the last element of the [queue]. It should only
+      be invoked by the domain that owns the [queue].
+            
+      @raises Empty if the [queue] is empty. *)
+
+(** {2 Stealer functions} *)
 
 val steal_exn : 'a t -> 'a
-(** [steal_exn q] removes and returns the last element from queue
-      [q]. It should only be invoked by domain which doesn't own the
-      queue [q].
+(** [steal_exn queue] removes and returns the first element of the [queue]. 
 
-      @raise Exit if the queue is empty.
-  *)
+        @raises Empty if the [queue] is empty. *)
 
 val steal_opt : 'a t -> 'a option
-(** [steal_opt q] removes and returns the last element from queue
-      [q], or returns [None] if the queue is empty. It should only be
-      invoked by domain which doesn't own the queue [q]. *)
+(** [steal_opt queue] removes and returns [Some] of the first element of the 
+      [queue], or returns [None] if the [queue] is empty. *)
+
+val steal_drop_exn : 'a t -> unit
+(** [steal_drop_exn queue] removes the first element of the [queue]. 
+            
+      @raises Empty if the [queue] is empty. *)
+
+(** {1 Examples} *)
+
+(** {2 Sequential example} 
+      An example top-level session:
+{[
+      # open Saturn.Work_stealing_deque
+      # let t : int t = of_list [1;2;3;4;5;6]
+      val t : int t = <abstr>
+      # pop_opt t
+      - : int option = Some 6
+      # steal_opt t
+      - : int option = Some 1
+      # drop_exn t
+      - : unit = ()
+      # pop_opt t
+      - : int option = Some 4
+      # steal_drop_exn t
+      - : unit = ()
+      # steal_exn t
+      - : int = 3
+      # steal_exn t
+      Exception: Saturn__Ws_deque.Empty.
+]}
+*)
+
+(** {2 Multicore example} 
+  Note: The barrier is used in this example solely to make the results more
+  interesting by increasing the likelihood of parallelism. Spawning a domain is 
+  a costly operation, especially compared to the relatively small amount of work
+  being performed here. In practice, using a barrier in this manner is unnecessary.
+
+
+{@ocaml non-deterministic=command[
+      # open Saturn.Work_stealing_deque
+      # let t : int t = create ()
+      val t : int t = <abstr>
+      # let barrier = Atomic.make 3
+      val barrier : int Atomic.t = <abstr>
+
+      # let owner () = 
+            Atomic.decr barrier;
+            while Atomic.get barrier <> 0 do Domain.cpu_relax () done;
+            for i = 1 to 10 do push t i; Domain.cpu_relax () done
+      val owner : unit -> unit = <fun>
+
+      # let stealer id () =
+            Atomic.decr barrier;
+            while Atomic.get barrier <> 0 do Domain.cpu_relax () done;
+
+            for _ = 1 to 5 do
+                  match steal_opt t with
+                  | None -> ()
+                  | Some v -> Format.printf "Stealer %s stole %d@." id v
+            done
+      val stealer : string -> unit -> unit = <fun>
+
+      # let stealerA = Domain.spawn (stealer "A")
+      val stealerA : unit Domain.t = <abstr>
+      # let stealerB = Domain.spawn (stealer "B")
+      val stealerB : unit Domain.t = <abstr>
+      # owner ()
+      Stealer A stole 1
+      Stealer B stole 2
+      Stealer A stole 3
+      Stealer B stole 4
+      Stealer A stole 5
+      Stealer A stole 7
+      Stealer B stole 6
+      Stealer A stole 8
+      Stealer B stole 9
+      Stealer B stole 10
+      - : unit = ()
+      # Domain.join stealerA; Domain.join stealerB
+      - : unit = ()
+]}
+*)
--- test/ws_deque/ws_deque_dscheck.ml
+++ test/ws_deque/ws_deque_dscheck.ml
@@ -1,9 +1,8 @@
 let drain_remaining queue =
-  let remaining = ref 0 in
+  let remaining = ref [] in
   (try
      while true do
-       Ws_deque.pop_exn queue |> ignore;
-       remaining := !remaining + 1
+       remaining := Ws_deque.pop_exn queue :: !remaining
      done
    with _ -> ());
   !remaining
@@ -36,31 +35,130 @@ let owner_stealer () =
 
       Atomic.final (fun () ->
           Atomic.check (fun () ->
-              let remaining = drain_remaining queue in
+              let remaining = drain_remaining queue |> List.length in
               remaining + !popped == total_items)))
 
-let popper_stealer () =
+let of_list_stealers () =
   Atomic.trace (fun () ->
-      let queue = Ws_deque.create () in
       let total_items = 3 in
-      for _ = 1 to total_items do
-        Ws_deque.push queue 0
-      done;
+      let queue = Ws_deque.of_list (List.init total_items (fun x -> x + 1)) in
 
       (* stealers *)
       let popped = ref 0 in
+      Atomic.spawn (fun () ->
+          Ws_deque.push queue (total_items + 1);
+          popped := Ws_deque.pop_exn queue);
+
+      let stolen = ref [] in
+      let stealer () =
+        match Ws_deque.steal_exn queue with
+        | exception _ -> ()
+        | v -> stolen := v :: !stolen
+      in
+      for _ = 1 to total_items - 1 do
+        Atomic.spawn stealer
+      done;
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () -> !popped = total_items + 1);
+          Atomic.check (fun () ->
+              List.sort Int.compare !stolen
+              = List.init (total_items - 1) (fun x -> x + 1));
+          Atomic.check (fun () -> drain_remaining queue = [ total_items ])))
+
+let popper_stealer () =
+  Atomic.trace (fun () ->
+      let total_items = 5 in
+      let queue = Ws_deque.of_list (List.init total_items (fun _ -> 0)) in
+
+      (* stealers *)
+      let popped = ref 0 in
+      Atomic.spawn (fun () ->
+          match Ws_deque.pop_opt queue with
+          | None -> ()
+          | _ -> popped := !popped + 1);
+
       let stealer () =
         match Ws_deque.steal_exn queue with
         | exception _ -> ()
         | _ -> popped := !popped + 1
       in
-      Atomic.spawn stealer |> ignore;
-      Atomic.spawn stealer |> ignore;
+      for _ = 1 to total_items - 2 do
+        Atomic.spawn stealer
+      done;
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              let remaining = drain_remaining queue |> List.length in
+              remaining = 1 && !popped = total_items - 1)))
+
+let popper_stealer_drop () =
+  Atomic.trace (fun () ->
+      let total_items = 5 in
+      let queue = Ws_deque.of_list (List.init total_items (fun _ -> 0)) in
+
+      (* stealers *)
+      let popped = ref 0 in
+      Atomic.spawn (fun () ->
+          match Ws_deque.drop_exn queue with
+          | exception Ws_deque.Empty -> ()
+          | _ -> popped := !popped + 1);
+
+      let stealer () =
+        match Ws_deque.steal_drop_exn queue with
+        | exception Ws_deque.Empty -> ()
+        | _ -> popped := !popped + 1
+      in
+      for _ = 1 to total_items - 2 do
+        Atomic.spawn stealer
+      done;
+
+      Atomic.final (fun () ->
+          Atomic.check (fun () ->
+              let remaining = drain_remaining queue |> List.length in
+              remaining = 1 && !popped = total_items - 1)))
+
+let owner_2_stealers () =
+  Atomic.trace (fun () ->
+      let queue = Ws_deque.create () in
+      let total_items = 6 in
+
+      let popped = ref [] in
+      (* owner thr *)
+      Atomic.spawn (fun () ->
+          for i = 1 to total_items do
+            Ws_deque.push queue i
+          done;
+          for _ = 1 to total_items / 3 do
+            match Ws_deque.pop_exn queue with
+            | exception _ -> ()
+            | v -> popped := v :: !popped
+          done);
+
+      let stolen1 = ref [] in
+      (* stealer *)
+      Atomic.spawn (fun () ->
+          for _ = 1 to total_items / 3 do
+            match Ws_deque.steal_exn queue with
+            | exception _ -> ()
+            | v -> stolen1 := v :: !stolen1
+          done);
+
+      let stolen2 = ref [] in
+      (* stealer *)
+      Atomic.spawn (fun () ->
+          for _ = 1 to total_items / 3 do
+            match Ws_deque.steal_exn queue with
+            | exception _ -> ()
+            | v -> stolen2 := v :: !stolen2
+          done);
 
       Atomic.final (fun () ->
           Atomic.check (fun () ->
               let remaining = drain_remaining queue in
-              remaining = 1 && !popped = 2)))
+              let total = !popped @ !stolen1 @ !stolen2 @ remaining in
+              List.sort Int.compare total
+              = List.init total_items (fun x -> x + 1))))
 
 let () =
   let open Alcotest in
@@ -69,8 +167,9 @@ let () =
       ( "basic",
         [
           test_case "1-owner-1-stealer" `Slow owner_stealer;
-          test_case "1-pusher-2-stealers" `Slow popper_stealer;
-          (* we'd really want to test cases with more threads here,
-             but dscheck is not optimized enough for that yet *)
+          test_case "1-popped-n-stealers" `Slow popper_stealer;
+          test_case "1-popped-n-stealers-drop" `Slow popper_stealer_drop;
+          test_case "1-owner-2-stealers" `Slow owner_2_stealers;
+          test_case "of_list-n-stealers" `Slow of_list_stealers;
         ] );
     ]
