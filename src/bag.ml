5f2954840ecaaed20266393bddb8f31d482cc9cb
Carine Morel <carine@tarides.com>
2024-12-03 16:25:25 +0100

    Add examples.

--- src/bag.ml
+++ src/bag.ml
@@ -9,12 +9,28 @@ type 'v t = (int, 'v) Htbl.t
 
 let create () = Htbl.create ~hashed_type:(module Key) ()
 
+(* *)
+
 let rec push t value =
   let key = Int64.to_int (Random.bits64 ()) in
   if not (Htbl.try_add t key value) then push t value
 
+(* *)
+
 exception Empty
 
-let rec pop_exn t =
-  let key = try Htbl.find_random_exn t with Not_found -> raise Empty in
-  try Htbl.remove_exn t key with Not_found -> pop_exn t
+type ('a, _) poly = Option : ('a, 'a option) poly | Value : ('a, 'a) poly
+
+let rec pop_as : type a r. a t -> (a, r) poly -> r =
+ fun t poly ->
+  match Htbl.find_random_exn t with
+  | key -> begin
+      match Htbl.remove_exn t key with
+      | value -> ( match poly with Option -> Some value | Value -> value)
+      | exception Not_found -> pop_as t poly
+    end
+  | exception Not_found -> (
+      match poly with Option -> None | Value -> raise Empty)
+
+let pop_exn t = pop_as t Value
+let pop_opt t = pop_as t Option
--- src/bag.mli
+++ src/bag.mli
@@ -1,4 +1,4 @@
-(** Randomized lock-free bag implemented using a hash table *)
+(** Randomized lock-free bag implemented using {!Saturn.Htbl} *)
 
 (** {1 API} *)
 
@@ -16,5 +16,47 @@ exception Empty
 
 val pop_exn : 'v t -> 'v
 (** [pop_exn bag] removes and returns a random element of the [bag].
- 
-  @raises Empty if the [bag] is empty. *)
+
+    @raise Empty if the [bag] is empty. *)
+
+val pop_opt : 'v t -> 'v option
+(** [pop_opt bag] removes and returns [Some] of a random element of the [bag] 
+and [None] if the [bag] is empty. *)
+
+(** {1 Example} 
+
+{[
+    # Random.init 0
+    - : unit = ()
+    # module Bag = Saturn.Bag
+    module Bag = Saturn.Bag
+    # let t : string Bag.t = Bag.create ()
+    val t : string Bag.t = <abstr>
+
+    # let planets = ["Mercury"; "Venus"; "Earth"; "Mars"; "Jupiter"; "Saturn"; "Uranus"; "Neptune"]
+    val planets : string list =
+      ["Mercury"; "Venus"; "Earth"; "Mars"; "Jupiter"; "Saturn"; "Uranus";
+       "Neptune"]
+    # List.iter (Bag.push t) planets
+    - : unit = ()
+    # Bag.pop_exn t
+    - : string = "Neptune"
+    # Bag.pop_opt t
+    - : string option = Some "Saturn"
+    # Bag.pop_exn t
+    - : string = "Mercury"
+    # Bag.pop_exn t
+    - : string = "Mars"
+    # Bag.pop_exn t
+    - : string = "Earth"
+    # Bag.pop_exn t
+    - : string = "Venus"
+    # Bag.pop_exn t
+    - : string = "Uranus"
+    # Bag.pop_exn t
+    - : string = "Jupiter"
+    # Bag.pop_exn t
+    Exception: Saturn__Bag.Empty.
+]} 
+
+*)
